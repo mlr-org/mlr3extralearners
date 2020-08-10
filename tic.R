618cb78abb2855d802c555df4aeb6ebf88bf79ac
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-08-10 17:34:28 +0100

    Update tic.R

--- tic.R
+++ tic.R
@@ -4,7 +4,7 @@ get_stage("install") %>%
 
   get_stage("script") %>%
     add_code_step(remotes::install_dev("mlr3")) %>%
-    add_code_step(devtools::test(filter = tic::ci_get_env("env.pkg"),
+    add_code_step(devtools::test(filter = tic::ci_get_env("PKG"),
                                  stop_on_failure = TRUE))
 
 } else if (ci_has_env("PARAMTEST")) {
