954a604e3b216e7c3c1c91e60f1bbc26a7514c2c
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-08-11 15:02:08 +0100

    Update tic.R

--- R/aaa.R
+++ R/aaa.R
@@ -1,28 +1,5 @@
 lrns_dict = mlr3misc::Dictionary$new()
 
-buid_learner_table = function() {
-  library(mlr3)
-  library(mlr3learners)
-  library(mlr3extralearners)
-  library(mlr3proba)
-
-  keys = mlr3::mlr_learners$keys()
-  all_lrns = suppressWarnings(mlr3::lrns(keys))
-  .mlr3learners = data.table::data.table(t(data.table::rbindlist(list(
-    mlr3misc::map(all_lrns, function(.x) {
-      idsplt = strsplit(.x$id, ".", TRUE)[[1]]
-      list(idsplt[[2]], idsplt[[1]], .x$id, strsplit(.x$man, "::", TRUE)[[1]][1],
-           .x$packages[1], .x$properties, .x$feature_types, .x$predict_types)
-    })))))
-
-  colnames(.mlr3learners) = c("name", "class", "id", "mlr3_package", "required_package",
-                              "properties", "feature_types", "predict_types")
-  .mlr3learners[, 1:4] = lapply(.mlr3learners[, 1:4], as.character)
-  save(.mlr3learners,
-       file = file.path(system.file("inst/data", package = "mlr3extralearners"),
-                        ".mlr3learners.rds"))
-}
-
 #' @title List Learners in mlr3verse
 #' @description Lists all learners, properties, and associated packages in a table that can be
 #' filtered and queried.
--- tic.R
+++ tic.R
@@ -1,3 +1,26 @@
+buid_learner_table = function() {
+  library(mlr3)
+  library(mlr3learners)
+  library(mlr3extralearners)
+  library(mlr3proba)
+
+  keys = mlr3::mlr_learners$keys()
+  all_lrns = suppressWarnings(mlr3::lrns(keys))
+  .mlr3learners = data.table::data.table(t(data.table::rbindlist(list(
+    mlr3misc::map(all_lrns, function(.x) {
+      idsplt = strsplit(.x$id, ".", TRUE)[[1]]
+      list(idsplt[[2]], idsplt[[1]], .x$id, strsplit(.x$man, "::", TRUE)[[1]][1],
+           .x$packages[1], .x$properties, .x$feature_types, .x$predict_types)
+    })))))
+
+  colnames(.mlr3learners) = c("name", "class", "id", "mlr3_package", "required_package",
+                              "properties", "feature_types", "predict_types")
+  .mlr3learners[, 1:4] = lapply(.mlr3learners[, 1:4], as.character)
+  save(.mlr3learners,
+       file = file.path(system.file("inst/data", package = "mlr3extralearners"),
+                        ".mlr3learners.rds"))
+}
+
 if (!ci_has_env("PARAMTEST")) {
   get_stage("install") %>%
     add_step(step_install_deps())
