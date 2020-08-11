2eea0ea9581a519931bd5ddaea696d1c68f5434a
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-08-11 16:24:09 +0100

    move table build function

--- tic.R
+++ tic.R
@@ -1,26 +1,3 @@
-buid_learner_table = function() {
-  library(mlr3)
-  library(mlr3learners)
-  library(mlr3extralearners)
-  library(mlr3proba)
-
-  keys = mlr3::mlr_learners$keys()
-  all_lrns = suppressWarnings(mlr3::lrns(keys))
-  .mlr3learners = data.table::data.table(t(data.table::rbindlist(list(
-    mlr3misc::map(all_lrns, function(.x) {
-      idsplt = strsplit(.x$id, ".", TRUE)[[1]]
-      list(idsplt[[2]], idsplt[[1]], .x$id, strsplit(.x$man, "::", TRUE)[[1]][1],
-           .x$packages[1], .x$properties, .x$feature_types, .x$predict_types)
-    })))))
-
-  colnames(.mlr3learners) = c("name", "class", "id", "mlr3_package", "required_package",
-                              "properties", "feature_types", "predict_types")
-  .mlr3learners[, 1:4] = lapply(.mlr3learners[, 1:4], as.character)
-  save(.mlr3learners,
-       file = file.path(system.file("inst/data", package = "mlr3extralearners"),
-                        ".mlr3learners.rds"))
-}
-
 if (!ci_has_env("PARAMTEST")) {
   get_stage("install") %>%
     add_step(step_install_deps())
@@ -31,7 +8,7 @@ if (!ci_has_env("PARAMTEST")) {
                                  stop_on_failure = TRUE))
 
   get_stage("after_success") %>%
-    add_code_step(buid_learner_table())
+    add_code_step(mlr3extralearners:::buid_learner_table())
 
 } else if (ci_has_env("PARAMTEST")) {
   # PARAMTEST
