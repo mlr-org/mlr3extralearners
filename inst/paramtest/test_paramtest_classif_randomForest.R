35d6cf56cf647eef13a5ac1deb4cace6aa15f98c
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-08-13 15:45:01 +0100

    fix randomforest

--- inst/paramtest/test_paramtest_classif_randomForest.R
+++ inst/paramtest/test_paramtest_classif_randomForest.R
@@ -1,5 +1,5 @@
 library(mlr3extralearners)
-install_learners("classif.randomforest")
+install_learners("classif.randomForest")
 
 test_that("classif.randomforest", {
   learner = lrn("classif.randomForest")
