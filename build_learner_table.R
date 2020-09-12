2a82321a5138eddd759fa4e4dfc0a1aa3116958a
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-09-12 14:49:56 +0100

    fix learner table

--- build_learner_table.R
+++ build_learner_table.R
@@ -8,13 +8,13 @@ library(mlr3proba)
 
 keys = mlr3::mlr_learners$keys()
 all_lrns = suppressWarnings(mlr3::lrns(keys))
-dt = data.table::data.table(t(data.table::rbindlist(list(
+mlr3learners_table = data.table::data.table(t(data.table::rbindlist(list(
   mlr3misc::map(all_lrns, function(.x) {
     idsplt = strsplit(.x$id, ".", TRUE)[[1]]
     list(idsplt[[2]], idsplt[[1]], .x$id, strsplit(.x$man, "::", TRUE)[[1]][1],
          .x$packages, .x$properties, .x$feature_types, .x$predict_types)
   })))))
-colnames(dt) = c("name", "class", "id", "mlr3_package", "required_packages",
+colnames(mlr3learners_table) = c("name", "class", "id", "mlr3_package", "required_packages",
                             "properties", "feature_types", "predict_types")
-dt[, 1:4] = lapply(dt[, 1:4], as.character)
-usethis::use_data(dt, internal = TRUE, overwrite = TRUE)
+mlr3learners_table[, 1:4] = lapply(mlr3learners_table[, 1:4], as.character)
+usethis::use_data(mlr3learners_table, internal = TRUE, overwrite = TRUE)
