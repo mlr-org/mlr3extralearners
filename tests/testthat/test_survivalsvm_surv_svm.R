03684c927acdda43ac8fb5dd465a665d1b58d265
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-09-30 21:24:35 +0100

    add check_replicable

--- tests/testthat/test_survivalsvm_surv_svm.R
+++ tests/testthat/test_survivalsvm_surv_svm.R
@@ -4,6 +4,6 @@ test_that("autotest", {
   learner = LearnerSurvSVM$new()
   learner$param_set$values = list(gamma.mu = 0.1)
   expect_learner(learner)
-  result = run_autotest(learner, exclude = "feat_single")
+  result = run_autotest(learner, exclude = "feat_single", check_replicable = FALSE)
   expect_true(result, info = result$error)
 })
