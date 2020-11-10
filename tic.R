a006966308a8411ff236c0aacd9e826e83ec18ba
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-11-10 09:00:49 +0000

    Update tic.R

--- tic.R
+++ tic.R
@@ -24,11 +24,15 @@ if (ci_get_env("TEST") == "Learner") {
 } else {
   do_package_checks()
 
+  get_stage("before_deploy") %>%
+    add_step(step_setup_push_deploy(branch = "master", orphan = FALSE))
+  
   get_stage("install") %>%
     add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
 
   do_pkgdown()
 
   get_stage("deploy") %>%
-    add_code_step(rmarkdown::render("README.Rmd"))
+    add_code_step(rmarkdown::render("README.Rmd")) %>%
+    add_step(step_do_push_deploy(commit_paths = c("README.md")))
 }
