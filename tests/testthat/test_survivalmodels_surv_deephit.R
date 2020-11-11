9778172c69db3ea4b925f2f2642fca29f5954d0d
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2020-11-11 18:10:28 +0000

    fix survivalmodels

--- tests/testthat/test_survivalmodels_surv_deephit.R
+++ tests/testthat/test_survivalmodels_surv_deephit.R
@@ -13,8 +13,21 @@ np$random$seed(1L)
 torch$manual_seed(1L)
 
 test_that("autotest", {
-  learner = LearnerSurvDeephit$new()
+  learner = lrn("surv.deephit", epochs = 50)
   expect_learner(learner)
-  result = run_autotest(learner, check_replicable = FALSE)
+  result = run_autotest(learner, check_replicable = FALSE, exclude = "sanity")
   expect_true(result, info = result$error)
+})
+
+test_that("manual sanity", {
+  learner = lrn("surv.deephit", epochs = 50)
+  x = runif(100,0,10)
+  task = TaskSurv$new("sanity",
+                      data.frame(x = x,
+                                 time = x + rnorm(100),
+                                 status = rbinom(100, 1, 0.5)),
+                      time = "time", event = "status")
+  p = learner$train(task)$predict(task)
+  expect_true(p$score() >= 0.5)
+  expect_equal(rank(-p$crank), unname(rank(p$distr$mean())))
 })
--- tests/testthat/test_survivalmodels_surv_dnnsurv.R
+++ tests/testthat/test_survivalmodels_surv_dnnsurv.R
@@ -6,6 +6,19 @@ test_that("autotest", {
   set.seed(10)
   learner <- lrn("surv.dnnsurv", cuts = 5)
   expect_learner(learner)
-  result <- run_autotest(learner, check_replicable = FALSE)
+  result <- run_autotest(learner, check_replicable = FALSE, exclude = "sanity")
   expect_true(result, info = result$error)
+})
+
+test_that("manual sanity", {
+  learner = lrn("surv.dnnsurv", epochs = 50)
+  x = runif(100,0,10)
+  task = TaskSurv$new("sanity",
+                      data.frame(x = x,
+                                 time = x + rnorm(100),
+                                 status = rbinom(100, 1, 0.5)),
+                      time = "time", event = "status")
+  p = learner$train(task)$predict(task)
+  expect_true(p$score() >= 0.5)
+  expect_equal(rank(-p$crank), unname(rank(p$distr$mean())))
 })
