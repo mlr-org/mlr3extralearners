5af3c642580914f34197e4baf745067013e45820
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2021-01-26 15:17:17 +0000

    remove devtools install

--- inst/paramtest/test_paramtest_catboost_regr_catboost.R
+++ inst/paramtest/test_paramtest_catboost_regr_catboost.R
@@ -1,5 +1,5 @@
 library(mlr3extralearners)
-devtools::install_url('https://github.com/catboost/catboost/releases/download/v0.24.1/catboost-R-Linux-0.24.1.tgz', # nolint
+remotes::install_url('https://github.com/catboost/catboost/releases/download/v0.24.1/catboost-R-Linux-0.24.1.tgz', # nolint
                       INSTALL_opts = c("--no-multiarch"))
 
 
