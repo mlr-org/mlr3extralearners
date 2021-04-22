182c0ba5e8a3baa470db923312ca6d0d9d90a996
Raphael Sonabend <raphael.sonabend.15@ucl.ac.uk>
2021-04-22 17:41:31 +0100

    Update test_RWeka_classif_PART.R

--- tests/testthat/test_RWeka_classif_PART.R
+++ tests/testthat/test_RWeka_classif_PART.R
@@ -6,6 +6,6 @@ test_that("autotest", {
   set.seed(1)
   learner = LearnerClassifPART$new()
   expect_learner(learner)
-  result = run_autotest(learner, exclude = "missings_each_row_binary")
+  result = run_autotest(learner, exclude = "missings_each_row")
   expect_true(result, info = result$error)
 })
