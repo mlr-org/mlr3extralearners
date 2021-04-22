9646f567a80a5ef1c7e49effa0dede3b38843a3f
Raphael Sonabend <raphaelsonabend@gmail.com>
2021-04-22 10:05:36 +0100

    add seeds for tests

--- tests/testthat/test_RWeka_classif_J48.R
+++ tests/testthat/test_RWeka_classif_J48.R
@@ -3,6 +3,7 @@ install_learners("classif.J48")
 skip_on_os("windows")
 
 test_that("autotest", {
+  set.seed(1)
   learner = LearnerClassifJ48$new()
   expect_learner(learner)
   result = run_autotest(learner)
