d5219db7185e7f3e55b9a84214fbf0d796a113b5
Michel Lang <michellang@gmail.com>
2021-05-20 21:42:36 +0200

    remove nnet (in mlr3learners

--- tests/testthat/test_nnet_classif_nnet.R
+++
@@ -1,8 +0,0 @@
-install_learners("classif.nnet")
-
-test_that("autotest", {
-  learner = LearnerClassifNnet$new()
-  expect_learner(learner)
-  result = run_autotest(learner)
-  expect_true(result, info = result$error)
-})
