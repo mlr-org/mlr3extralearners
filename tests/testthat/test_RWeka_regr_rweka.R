28f358c1b3f6426495476b8200ffb94acb7e6cab
RaphaelS1 <raphaelsonabend@gmail.com>
2021-07-06 19:00:21 +0100

    fix params

--- tests/testthat/test_RWeka_regr_rweka.R
+++ tests/testthat/test_RWeka_regr_rweka.R
@@ -2,6 +2,8 @@ install_learners("regr.rweka")
 
 skip_on_os("windows")
 
+set.seed(12)
+
 test_that("autotest - IBk", {
   learner = lrn("regr.rweka", model = "IBk")
   expect_learner(learner)
@@ -12,6 +14,6 @@ test_that("autotest - IBk", {
 test_that("autotest - M5Rules", {
   learner = lrn("regr.rweka", model = "M5Rules")
   expect_learner(learner)
-  result = run_autotest(learner)
+  result = run_autotest(learner, exclude = "sanity")
   expect_true(result, info = result$error)
 })
