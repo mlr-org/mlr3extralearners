61184781342fd36b9cbe78dbeb41917ea91a3461
RaphaelS1 <raphaelsonabend@gmail.com>
2021-07-08 16:57:40 +0100

    remove liblinear tests

--- tests/testthat/test_LiblineaR_classif_liblinearl1l2svc.R
+++
@@ -1,8 +0,0 @@
-install_learners("classif.liblinearl1l2svc")
-
-test_that("autotest", {
-  learner = LearnerClassifLiblineaRL1L2SVC$new()
-  expect_learner(learner)
-  result = run_autotest(learner)
-  expect_true(result, info = result$error)
-})
--- tests/testthat/test_LiblineaR_classif_liblinearl2l1svc.R
+++
@@ -1,8 +0,0 @@
-install_learners("classif.liblinearl2l1svc")
-
-test_that("autotest", {
-  learner = LearnerClassifLiblineaRL2L1SVC$new()
-  expect_learner(learner)
-  result = run_autotest(learner)
-  expect_true(result, info = result$error)
-})
