0434147741f2724e076c0050f308e8087dcf0170
Raphael Sonabend <raphaelsonabend@gmail.com>
2021-08-31 14:11:48 +0100

    install devtools

--- R/fn_install_catboost.R
+++ R/fn_install_catboost.R
@@ -5,8 +5,8 @@
 #' @param os `(character(1))` \cr
 #' Operating system to install on, if `NULL` automatically detected
 #' @param install_required `(logical(1))` \cr
-#' If `TRUE` (default) and version is `NULL` then installs {curl} and
-#' {jsonlite} if not already installed.
+#' If `TRUE` (default) then installs required packages: {curl}, {jsonlite},
+#' {devtools}
 #' @param INSTALL_opts `(character())` \cr
 #' Passed to [devtools::install_url]
 #' @param ... `ANY` \cr
@@ -18,6 +18,7 @@ install_catboost <- function(version = NULL, os = NULL,
                                               "--no-test-load"), ...) {
 
   if (is.null(version)) {
+
     if (requireNamespace("jsonlite", quietly = TRUE) && install_required) {
       utils::install.packages("jsonlite", repos = "https://cloud.r-project.org")
     }
@@ -40,6 +41,10 @@ install_catboost <- function(version = NULL, os = NULL,
   url <- sprintf(
     "https://github.com/catboost/catboost/releases/download/v%s/catboost-R-%s-%s.tgz",
     version, os, version)
+
+  if (requireNamespace("devtools", quietly = TRUE) && install_required) {
+    utils::install.packages("devtools", repos = "https://cloud.r-project.org")
+  }
 
   devtools::install_url(url, INSTALL_opts = INSTALL_opts, ...)
 }
