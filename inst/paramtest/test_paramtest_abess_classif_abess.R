abf1d0a30d4d0ee0e4778fe099609dec5b3b9b4f
rlwanggg <76217775+rlwanggg@users.noreply.github.com>
2021-11-17 00:57:41 +0800

    Add files via upload

---
+++ inst/paramtest/test_paramtest_abess_classif_abess.R
@@ -0,0 +1,31 @@
+library(mlr3extralearners)
+install_learners("classif.abess")
+
+test_that("classif.abess train", {
+  learner = lrn("classif.abess")
+  fun = abess::abess
+  exclude = c(
+    "object", # handled internally
+    "data" # handled internally
+  )
+
+  ParamTest = run_paramtest(learner, fun, exclude)
+  expect_true(ParamTest, info = paste0(
+    "Missing parameters:",
+    paste0("- '", ParamTest$missing, "'", collapse = "")))
+})
+
+test_that("classif.abess predict", {
+  learner = lrn("classif.abess")
+  fun = abess:::predict # nolint
+    exclude = c(
+      "object", # handled internally
+      "data", # handled internally
+      "newdata" # handled internally
+    )
+
+  ParamTest = run_paramtest(learner, fun, exclude)
+  expect_true(ParamTest, info = paste0(
+    "Missing parameters:",
+    paste0("- '", ParamTest$missing, "'", collapse = "")))
+})
---
+++ inst/paramtest/test_paramtest_abess_regr_abess.R
@@ -0,0 +1,31 @@
+library(mlr3extralearners)
+install_learners("regr.abess")
+
+test_that("regr.abess train", {
+  learner = lrn("regr.abess")
+  fun = abess::abess
+  exclude = c(
+    "object", # handled internally
+    "data" # handled internally
+  )
+
+  ParamTest = run_paramtest(learner, fun, exclude)
+  expect_true(ParamTest, info = paste0(
+    "Missing parameters:",
+    paste0("- '", ParamTest$missing, "'", collapse = "")))
+})
+
+test_that("regr.abess predict", {
+  learner = lrn("regr.abess")
+  fun = abess:::predict # nolint
+    exclude = c(
+      "object", # handled internally
+      "data", # handled internally
+      "newdata" # handled internally
+    )
+
+  ParamTest = run_paramtest(learner, fun, exclude)
+  expect_true(ParamTest, info = paste0(
+    "Missing parameters:",
+    paste0("- '", ParamTest$missing, "'", collapse = "")))
+})
