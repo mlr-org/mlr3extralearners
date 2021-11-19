309810335c28500641b44e9f8e6db37223f8ba1a
Sebastian Fischer <sebf.fischer@gmail.com>
2021-11-19 14:23:08 +0100

    fix AdaBoostM1 Test

--- inst/paramtest/test_paramtest_RWeka_classif_AdaBoostM1.R
+++ inst/paramtest/test_paramtest_RWeka_classif_AdaBoostM1.R
@@ -4,39 +4,27 @@ mlr3extralearners::install_learners("classif.AdaBoostM1")
 test_that("classif.AdaBoostM1", {
   learner = lrn("classif.AdaBoostM1")
   fun = RWeka::AdaBoostM1
-  exclude = c(
-    "formula", # handled via mlr3
-    "data", # handled via mlr3
-    "control" # handled to RWeka::Weka_Control
-  )
+  exclude = weka_control_args(RWeka::AdaBoostM1)
+  # formula and data are handled via mlr3
+  # mlr3 does not have the `control` argument because the parameters can be specified directly
+  exclude = c("formula", "data", "control", exclude)
   ParamTest = run_paramtest(learner, fun, exclude)
-  expect_true(ParamTest, info = paste0(
-    "\nMissing parameters:\n",
-    paste0("- '", ParamTest$missing, "'", collapse = "\n")))
+  expect_true(ParamTest)
 })
 
 test_that("Weka_control AdaBoostM1", {
+  # Here we test that the learner implements those arguments that are passed via the
+  # control argument to RWeka::AdaBoostM1
   learner = lrn("classif.AdaBoostM1")
-  fun = RWeka::Weka_control
-  exclude = c(
-    character(0L)
-  )
-  ParamTest = run_paramtest(learner, fun, exclude)
-  expect_true(ParamTest, info = paste0(
-    "\nMissing parameters:\n",
-    paste0("- '", ParamTest$missing, "'", collapse = "\n")))
+  control_args = weka_control_args(RWeka::AdaBoostM1)
+  expect_true(all(control_args %in% learner$param_set$ids()))
 })
 
-test_that("predict classif.AdaBoostM1", {
-  learner = lrn("classif.AdaBoostM1")
-  fun = RWeka:::predict.Weka_classifier
-  exclude = c(
-    "object", # handled via mlr3
-    "newdata", # handled via mlr3
-    "type" # handled via mlr3
-  )
-  ParamTest = run_paramtest(learner, fun, exclude)
-  expect_true(ParamTest, info = paste0(
-    "Missing parameters:",
-    paste0("- '", ParamTest$missing, "'", collapse = ",")))
+
+test_that("Parameters can be set", {
+  learner1 = lrn("classif.AdaBoostM1")
+  learner1$param_set$values = learner1$param_set$default
+  task = tsk("iris")
+  expect_error(learner$train(task), NA)
 })
+
