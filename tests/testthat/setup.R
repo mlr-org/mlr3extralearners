26facb2297793153bde5d8d5109555e7022094a8
Sebastian Fischer <sebf.fischer@gmail.com>
2021-11-25 14:30:30 +0100

    correct helper and rename to setup

---
+++ tests/testthat/setup.R
@@ -0,0 +1,25 @@
+library(checkmate)
+library(mlr3extralearners)
+
+lapply(list.files(system.file("testthat", package = "mlr3"),
+  pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
+
+load_tests = function(key) {
+  class = strsplit(mlr3::lrn(key)$id, ".", TRUE)[[1]][[1]]
+
+  if (class %in% c("surv", "dens")) {
+    if (!requireNamespace("mlr3proba", quietly = TRUE)) {
+      remotes::install_github("mlr-org/mlr3proba", upgrade = "always")
+    }
+    invisible(lapply(list.files(system.file("testthat", package = "mlr3proba"),
+                      pattern = "^helper.*\\.[rR]", full.names = TRUE), source))
+    require("mlr3proba")
+  } else if (class == "clust") {
+    if (!requireNamespace("mlr3cluster", quietly = TRUE)) {
+      remotes::install_github("mlr-org/mlr3cluster", upgrade = "always")
+    }
+    invisible(lapply(list.files(system.file("testthat", package = "mlr3cluster"),
+                      pattern = "^helper.*\\.[rR]", full.names = TRUE), source))
+    require("mlr3cluster")
+  }
+}
