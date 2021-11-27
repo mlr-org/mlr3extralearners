e04758b6ab64be43cfd4bec2228226993b4f331d
rlwanggg <76217775+rlwanggg@users.noreply.github.com>
2021-11-27 10:37:28 +0800

    Update test_paramtest_abess_regr_abess.R

--- inst/paramtest/test_paramtest_abess_regr_abess.R
+++ inst/paramtest/test_paramtest_abess_regr_abess.R
@@ -1,6 +1,6 @@
-#library(mlr3extralearners)
+library(mlr3extralearners)
 if (!requireNamespace("abess", quietly = TRUE)) {
-#  library(devtools)
+  library(devtools)
   devtools::install_github(repo = "abess-team/abess", subdir = "R-package")
 }
 mlr3extralearners::install_learners("regr.abess")
@@ -9,27 +9,40 @@ test_that("regr.abess train", {
   learner = lrn("regr.abess")
   fun = abess::abess
   exclude = c(
-    "object", # handled internally
-    "data" # handled internally
+    "formula",# handled internally
+    "model", # handled internally
+    "data", # handled internally
+    "weights", # handled by task
+    "subset", # handled by task
+    "na.action", # handled internally
+    "method", # handled internally
+    "x", # handled internally
+    "y", # handled internally
+    "parms", # handled internally
+    "control", # handled internally
+    "cost" # handled internally
   )
-
+  
   ParamTest = run_paramtest(learner, fun, exclude)
   expect_true(ParamTest, info = paste0(
     "Missing parameters:",
-    paste0("- '", ParamTest$missing, "'", collapse = "")))
+    paste0("- '", ParamTest$missing, "'", collapse = "
+")))
 })
 
 test_that("regr.abess predict", {
   learner = lrn("regr.abess")
-  fun = abess:::predict # nolint
-    exclude = c(
-      "object", # handled internally
-      "data", # handled internally
-      "newdata" # handled internally
-    )
-
+  fun = abess:::predict.abess
+  exclude = c(
+    "object", # handled internally
+    "newx", # handled internally
+    "type", # handled internally
+    "na.action" # handled internally
+  )
+  
   ParamTest = run_paramtest(learner, fun, exclude)
   expect_true(ParamTest, info = paste0(
     "Missing parameters:",
-    paste0("- '", ParamTest$missing, "'", collapse = "")))
+    paste0("- '", ParamTest$missing, "'", collapse = "
+")))
 })
