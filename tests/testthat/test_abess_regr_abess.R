2b901052d480412b0355b34d880b41752776c7ed
rlwanggg <76217775+rlwanggg@users.noreply.github.com>
2021-11-27 10:38:10 +0800

    Delete test_abess_regr_abess.R

--- tests/testthat/test_abess_regr_abess.R
+++
@@ -1,9 +0,0 @@
-install_learners("regr.abess")
-load_tests("regr.abess")
-
-test_that("autotest", {
-  learner = lrn("regr.abess")
-  expect_learner(learner)
-  result = run_autotest(learner)
-  expect_true(result, info = result$error)
-})
