8fd01918cef11efe0778b127855c4b8e96348447
rlwanggg <76217775+rlwanggg@users.noreply.github.com>
2021-11-27 10:37:52 +0800

    Delete test_abess_classif_abess.R

--- tests/testthat/test_abess_classif_abess.R
+++
@@ -1,9 +0,0 @@
-install_learners("classif.abess")
-load_tests("classif.abess")
-
-test_that("autotest", {
-  learner = lrn("classif.abess")
-  expect_learner(learner)
-  result = run_autotest(learner)
-  expect_true(result, info = result$error)
-})
