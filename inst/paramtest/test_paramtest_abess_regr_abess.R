09bc2bfbb5d36c0d63f55639922730fb2d6de14c
rlwanggg <76217775+rlwanggg@users.noreply.github.com>
2021-11-28 23:21:22 +0800

    Update test_paramtest_abess_regr_abess.R

--- inst/paramtest/test_paramtest_abess_regr_abess.R
+++ inst/paramtest/test_paramtest_abess_regr_abess.R
@@ -4,43 +4,79 @@ if (!requireNamespace("abess", quietly = TRUE)) {
 mlr3extralearners::install_learners("regr.abess")
 
 test_that("regr.abess train", {
-  learner = mlr3extralearners::lrn("regr.abess")
+  learner = lrn("regr.abess")
   fun = abess::abess
   exclude = c(
-    "formula",# handled internally
-    "model", # handled internally
-    "data", # handled internally
-    "weights", # handled by task
-    "subset", # handled by task
-    "na.action", # handled internally
-    "method", # handled internally
     "x", # handled internally
-    "y", # handled internally
-    "parms", # handled internally
-    "control", # handled internally
-    "cost" # handled internally
+    "family",
+    "tune.path",
+    "tune.type",
+    "normalize",
+    "c.max",
+    "support.size",
+    "lambda",
+    "always.include",
+    "group.index",
+    "splicing.type",
+    "max.splicing.iter",
+    "screening.num",
+    "important.search",
+    "warm.start",
+    "nfolds",
+    "newton",
+    "newton.thresh",
+    "max.newton.iter",
+    "early.stop",
+    "num.threads",
+    "seed",
+    "subset",
+    "na.action",
+    "weight",
+    "cov.update"
   )
 
   ParamTest = run_paramtest(learner, fun, exclude)
   expect_true(ParamTest, info = paste0(
     "Missing parameters:",
-    paste0("- '", ParamTest$missing, "'", collapse = "
-")))
+    paste0("- '", ParamTest$missing, "'", collapse = "")))
 })
 
 test_that("regr.abess predict", {
-  learner = mlr3extralearners::lrn("regr.abess")
-  fun = abess:::predict.abess
+  learner = lrn("regr.abess")
+  fun = abess:::predict.abess # nolint
   exclude = c(
-    "object", # handled internally
+    "object", # handsetled internally
     "newx", # handled internally
     "type", # handled internally
-    "na.action" # handled internally
+    "family",
+    "tune.path",
+    "tune.type",
+    "normalize",
+    "c.max",
+    "support.size",
+    "lambda",
+    "always.include",
+    "group.index",
+    "splicing.type",
+    "max.splicing.iter",
+    "screening.num",
+    "important.search",
+    "warm.start",
+    "nfolds",
+    "newton",
+    "newton.thresh",
+    "max.newton.iter",
+    "early.stop",
+    "num.threads",
+    "seed",
+    "subset",
+    "na.action",
+    "cov.update",
+    "weight"
   )
 
   ParamTest = run_paramtest(learner, fun, exclude)
   expect_true(ParamTest, info = paste0(
     "Missing parameters:",
-    paste0("- '", ParamTest$missing, "'", collapse = "
-")))
+    paste0("- '", ParamTest$missing, "'", collapse = "")))
 })
