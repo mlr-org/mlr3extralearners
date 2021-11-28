2ac6c1a28414ce4cb3aa3915f74500fcccf6bdf9
rlwanggg <76217775+rlwanggg@users.noreply.github.com>
2021-11-28 23:40:25 +0800

    Update test_abess_classif_abess.R

--- tests/testthat/test_abess_classif_abess.R
+++ tests/testthat/test_abess_classif_abess.R
@@ -3,8 +3,6 @@ load_tests("classif.abess")
 
 test_that("autotest", {
   learner = lrn("classif.abess")
-  # learner = LearnerClassifAbess$new()
-  # learner$param_set$values()
   expect_learner(learner)
   result = run_autotest(learner, predict_types = "response")
   expect_true(result, info = result$error)
