4659720c2f082e3918f49b15def76cbbaf05d25e
rlwanggg <76217775+rlwanggg@users.noreply.github.com>
2021-11-30 21:27:28 +0800

    exclude the feat_single case

--- tests/testthat/test_abess_regr_abess.R
+++ tests/testthat/test_abess_regr_abess.R
@@ -4,6 +4,6 @@ load_tests("regr.abess")
 test_that("autotest", {
   learner = lrn("regr.abess")
   expect_learner(learner)
-  result = run_autotest(learner)
+  result = run_autotest(learner, exclude = "feat_single")
   expect_true(result, info = result$error)
 })
