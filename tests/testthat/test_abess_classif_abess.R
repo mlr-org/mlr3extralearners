2994afcc3701859629ca04155d0820157c154c31
rlwanggg <76217775+rlwanggg@users.noreply.github.com>
2021-11-30 21:26:48 +0800

    exclude the feat_single case

--- tests/testthat/test_abess_classif_abess.R
+++ tests/testthat/test_abess_classif_abess.R
@@ -4,6 +4,6 @@ load_tests("classif.abess")
 test_that("autotest", {
   learner = lrn("classif.abess")
   expect_learner(learner)
-  result = run_autotest(learner, predict_types = "response")
+  result = run_autotest(learner, predict_types = "response", exclude = "feat_single")
   expect_true(result, info = result$error)
 })
