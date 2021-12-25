5c7b4b2e56324b9278255b9945586f2ae66fd0a4
Sebastian Fischer <sebf.fischer@gmail.com>
2021-12-25 20:23:56 +0100

    Fixed classif glmboost test

--- tests/testthat/test_mboost_classif_glmboost.R
+++ tests/testthat/test_mboost_classif_glmboost.R
@@ -9,30 +9,18 @@ test_that("autotest", {
 
 test_that("classif.glmboost", {
   learner = lrn("classif.glmboost")
-  fun = mboost:::glmboost.formula
+  fun_list = list(mboost:::glmboost.formula, mboost::boost_control)
   exclude = c(
     "formula", # handled via mlr3
     "data", # handled via mlr3
     "weights", # handled via mlr3
     "control", # handled to mboost::boost_control
-    "..." # not used
+    "center", # deprecated
+     "custom.family", # provide custom family
+    "link", # mboost::Binomial
+    "type" # mboost::Binomial
   )
 
-  ParamTest = run_paramtest(learner, fun, exclude)
-  expect_true(ParamTest, info = paste0(
-    "\nMissing parameters:\n",
-    paste0("- '", ParamTest$missing, "'", collapse = "\n")))
-})
-
-test_that("classif.glmboost_boost_control", {
-  learner = lrn("classif.glmboost")
-  fun = mboost::boost_control
-  exclude = c(
-    "center" # deprecated
-  )
-
-  ParamTest = run_paramtest(learner, fun, exclude)
-  expect_true(ParamTest, info = paste0(
-    "\nMissing parameters:\n",
-    paste0("- '", ParamTest$missing, "'", collapse = "\n")))
+  paramtest = run_paramtest(learner, fun_list, exclude)
+  expect_paramtest(paramtest)
 })
