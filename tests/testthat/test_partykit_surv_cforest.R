1512dd2b2c5dcb6fca8a3a3cf7970a05226dfc04
Sebastian Fischer <sebf.fischer@gmail.com>
2022-01-08 09:57:53 +0100

    partykit tests

--- tests/testthat/test_partykit_surv_cforest.R
+++ tests/testthat/test_partykit_surv_cforest.R
@@ -4,7 +4,7 @@ load_tests("surv.cforest")
 test_that("autotest", {
   learner = LearnerSurvCForest$new()
   expect_learner(learner)
-  result = run_autotest(learner, check_replicable = FALSE, N = 10)
+  result = run_autotest(learner, check_replicable = FALSE, N = 100)
   expect_true(result, info = result$error)
 })
 
