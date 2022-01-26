27cfeb2698e53baa00077c520740782a2eb11ef7
Sebastian Fischer <sebf.fischer@gmail.com>
2022-01-26 12:08:18 +0100

    Exclude utf8 tests for some learners

--- tests/testthat/test_mboost_surv_mboost.R
+++ tests/testthat/test_mboost_surv_mboost.R
@@ -5,7 +5,7 @@ test_that("autotest ph", {
   learner = lrn("surv.mboost", baselearner = "bols", family = "coxph", center = TRUE)
   expect_learner(learner)
   # weights are fine for all predict types except 'distr'
-  result = run_autotest(learner, exclude = "weights", check_replicable = FALSE)
+  result = run_autotest(learner, exclude = c("weights", "utf8_feature_names"), check_replicable = FALSE)
   expect_true(result, info = result$error)
 })
 
@@ -13,6 +13,6 @@ test_that("autotest aft", {
   learner = lrn("surv.mboost", family = "loglog", baselearner = "bols", center = TRUE)
   expect_learner(learner)
   # weights are fine for all predict types except 'distr'
-  result = run_autotest(learner, exclude = "weights", check_replicable = FALSE)
+  result = run_autotest(learner, exclude = c("weights", "utf8_feature_names"), check_replicable = FALSE)
   expect_true(result, info = result$error)
 })
