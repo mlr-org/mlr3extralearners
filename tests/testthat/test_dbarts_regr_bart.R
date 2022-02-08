fbe7a35ecc5bf32d5abce1743c3b592c35d09ca4
Sebastian Fischer <sebf.fischer@gmail.com>
2022-02-08 12:44:36 +0100

    Remove utf8 name test for dbart

--- tests/testthat/test_dbarts_regr_bart.R
+++ tests/testthat/test_dbarts_regr_bart.R
@@ -8,6 +8,6 @@ test_that("autotest", {
     ntree = 5L)
 
   expect_learner(learner)
-  result = run_autotest(learner)
+  result = run_autotest(learner, exclude = "utf8_feature_names")
   expect_true(result, info = result$error)
 })
