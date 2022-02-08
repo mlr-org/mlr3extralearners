9a0e4bfe2906353594af4c963873e5cc0f8d8604
Sebastian Fischer <sebf.fischer@gmail.com>
2022-02-08 21:00:35 +0100

    fix mboost tests

--- tests/testthat/test_mboost_classif_gamboost.R
+++ tests/testthat/test_mboost_classif_gamboost.R
@@ -4,6 +4,6 @@ test_that("autotest", {
   learner = LearnerClassifGAMBoost$new()
   learner$param_set$values = list(baselearner = "btree", family = "Binomial")
   expect_learner(learner)
-  result = run_autotest(learner)
+  result = run_autotest(learner, exclude = "utf8_feature_names")
   expect_true(result, info = result$error)
 })
