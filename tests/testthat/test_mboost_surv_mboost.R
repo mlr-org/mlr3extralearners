ab8d9c80ab0046def5296dcdbeb87798065c2adf
Sebastian Fischer <sebf.fischer@gmail.com>
2022-02-08 09:25:36 +0100

    fix surv mboost test (accidentally removed test)

--- tests/testthat/test_mboost_surv_mboost.R
+++ tests/testthat/test_mboost_surv_mboost.R
@@ -8,3 +8,11 @@ test_that("autotest ph", {
   result = run_autotest(learner, exclude = "weights", check_replicable = FALSE)
   expect_true(result, info = result$error)
 })
+
+test_that("autotest aft", {
+  learner = lrn("surv.mboost", family = "loglog", baselearner = "bols", center = TRUE)
+  expect_learner(learner)
+  # weights are fine for all predict types except 'distr'
+  result = run_autotest(learner, exclude = "weights|utf8_feature_names", check_replicable = FALSE)
+  expect_true(result, info = result$error)
+})
