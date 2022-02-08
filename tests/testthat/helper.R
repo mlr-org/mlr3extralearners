0a45a494fdbe06ddcce402042a8c982aa2ea8209
Sebastian Fischer <sebf.fischer@gmail.com>
2022-02-08 09:52:25 +0100

    undo change of helper.R

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,3 +1,25 @@
-library(mlr3extralearners)
+library(checkmate)
+library(mlr3)
+
 lapply(list.files(system.file("testthat", package = "mlr3"),
-  pattern = "helper_autotest", full.names = TRUE), source)
+  pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
+
+load_tests = function(key) {
+  class = strsplit(mlr3::lrn(key)$id, ".", TRUE)[[1]][[1]]
+
+  if (class %in% c("surv", "dens")) {
+    if (!requireNamespace("mlr3proba", quietly = TRUE)) {
+      remotes::install_github("mlr-org/mlr3proba", upgrade = "always")
+    }
+    invisible(lapply(list.files(system.file("testthat", package = "mlr3proba"),
+                      pattern = "^helper.*\\.[rR]", full.names = TRUE), source))
+    require("mlr3proba")
+  } else if (class == "clust") {
+    if (!requireNamespace("mlr3cluster", quietly = TRUE)) {
+      remotes::install_github("mlr-org/mlr3cluster", upgrade = "always")
+    }
+    invisible(lapply(list.files(system.file("testthat", package = "mlr3cluster"),
+                      pattern = "^helper.*\\.[rR]", full.names = TRUE), source))
+    require("mlr3cluster")
+  }
+}
