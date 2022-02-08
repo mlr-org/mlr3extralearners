8207cfbe55f521642be50b93e7f93aa1341f3dc8
Sebastian Fischer <sebf.fischer@gmail.com>
2022-02-08 21:20:27 +0100

    mboost surv test

--- tests/testthat/test_mboost_surv_mboost.R
+++ tests/testthat/test_mboost_surv_mboost.R
@@ -5,7 +5,7 @@ test_that("autotest ph", {
   learner = lrn("surv.mboost", baselearner = "bols", family = "coxph", center = TRUE)
   expect_learner(learner)
   # weights are fine for all predict types except 'distr'
-  result = run_autotest(learner, exclude = "weights", check_replicable = FALSE)
+  result = run_autotest(learner, exclude = "weights|utf8_feature_names", check_replicable = FALSE)
   expect_true(result, info = result$error)
 })
 
