7b10aecea8e12c8af3916c673329bdc93412818f
Sebastian Fischer <sebf.fischer@gmail.com>
2022-04-21 21:50:54 +0200

    bug: obtain pars before using them (blackboost)

--- R/learner_mboost_surv_blackboost.R
+++ R/learner_mboost_surv_blackboost.R
@@ -164,13 +164,12 @@ LearnerSurvBlackBoost = R6Class("LearnerSurvBlackBoost",
     },
 
     .predict = function(task) {
-
+      pars = self$param_set$get_values(tags = "predict")
       newdata = task$data(cols = task$feature_names)
       # predict linear predictor
       lp = as.numeric(mlr3misc::invoke(predict, self$model, newdata = newdata, type = "link",
         .args = pars
       ))
-      pars = self$param_set$get_values(tags = "predict")
 
       # predict survival
       if (is.null(self$param_set$values$family) || self$param_set$values$family == "coxph") {
