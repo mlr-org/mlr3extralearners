9f09fcbc643ea106c98567027a02bc3f3233c2d3
Sebastian Fischer <sebf.fischer@gmail.com>
2022-04-21 20:49:25 +0200

    Fix Failing tests (#190)
    * chore: add new parameters for kde and rfsrc
    
    * temporarily disable feat_all test for obliqeRSF (failed in $score() stage, because issue only happened in CI and could not be reproduced

--- R/learner_randomForestSRC_regr_rfsrc.R
+++ R/learner_randomForestSRC_regr_rfsrc.R
@@ -81,7 +81,8 @@ LearnerRegrRandomForestSRC = R6Class("LearnerRegrRandomForestSRC",
           default = "train", levels = c("train", "test"),
           tags = "predict"),
         ptn.count = p_int(default = 0L, lower = 0L, tags = "predict"),
-        cores = p_int(default = 1L, lower = 1L, tags = c("train", "predict"))
+        cores = p_int(default = 1L, lower = 1L, tags = c("train", "predict", "threads")),
+        save.memory = p_lgl(default = FALSE, tags = "train")
       )
 
       super$initialize(
