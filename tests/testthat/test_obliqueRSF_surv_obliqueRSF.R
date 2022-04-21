5039924d1372472c2326af5faffe4366e2936b85
Sebastian Fischer <sebf.fischer@gmail.com>
2022-04-21 17:38:58 +0200

    ...

--- tests/testthat/test_obliqueRSF_surv_obliqueRSF.R
+++ tests/testthat/test_obliqueRSF_surv_obliqueRSF.R
@@ -1,16 +1,10 @@
 install_learners("surv.obliqueRSF")
 load_tests("surv.obliqueRSF")
 
-learner = lrn("surv.obliqueRSF", compute_oob_predictions = TRUE)
-expect_learner(learner)
-task = generate_tasks(learner)$feat_all
-learner$train(task)
-p = learner$predict(task)
-print(p)
-print(p$score(msr("surv.cindex")))
-
-# test_that("autotest", {
-#
-#   # result = run_autotest(learner, exclude = "single", N = 30, check_replicable = FALSE)
-#   # expect_true(result, info = result$error)
-# })
+test_that("autotest", {
+  set.seed(1) # there was presumable a weird bug
+  learner = lrn("surv.obliqueRSF", compute_oob_predictions = TRUE)
+  expect_learner(learner)
+  result = run_autotest(learner, exclude = "single", N = 30, check_replicable = FALSE)
+  expect_true(result, info = result$error)
+})
