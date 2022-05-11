bb4f25c1fe85ff5c8688097fee161a543f97deb3
Sebastian Fischer <sebf.fischer@gmail.com>
2022-05-11 18:51:06 +0200

    fix: Fix M5Rules sanity test

--- tests/testthat/test_RWeka_regr_M5Rules.R
+++ tests/testthat/test_RWeka_regr_M5Rules.R
@@ -5,6 +5,8 @@ skip_on_os("windows")
 test_that("autotest", {
   learner = LearnerRegrM5Rules$new()
   expect_learner(learner)
-  result = run_autotest(learner, exclude = "sanity")
+  # these parameters have to be set as otherwise the sanity check does not run
+  learner$param_set$values = list(M = 1L, N = TRUE, U = TRUE)
+  result = run_autotest(learner)
   expect_true(result, info = result$error)
 })
