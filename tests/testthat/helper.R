cd13aaf99929b78b7d1e4254967990f0ad4c1267
Sebastian Fischer <sebf.fischer@gmail.com>
2022-08-08 08:48:05 +0200

    revert last commit, but dont load mlr3cluster

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -2,7 +2,6 @@ library(checkmate)
 library(mlr3)
 library(mlr3extralearners)
 library(mlr3proba)
-library(mlr3cluster)
 
 lapply(list.files(system.file("testthat", package = "mlr3"),
   pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
