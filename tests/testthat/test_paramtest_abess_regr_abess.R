39ef37666def4f3622652af1de99e49d34895a3b
bbayukari <17bbayukari@gmail.com>
2022-08-27 18:27:00 +0800

    modify test_paramtest_abess_regr_abess.R

--- tests/testthat/test_paramtest_abess_regr_abess.R
+++ tests/testthat/test_paramtest_abess_regr_abess.R
@@ -1,13 +1,15 @@
 test_that("regr.abess train", {
   learner = lrn("regr.abess")
-  fun = abess::abess
+  fun = abess:::abess.default
   exclude = c(
-    "weight", # handled by mlr3
+    "x", # handled by mlr3
+    "y", # handled by mlr3
+    "weight" # handled by mlr3
   )
 
   # note that you can also pass a list of functions in case $.train calls more than one
   # function, e.g. for control arguments
-  paramtest = run_paramtest(learner, fun, exclude)
+  paramtest = run_paramtest(learner, fun, exclude, tag = 'train')
   expect_paramtest(paramtest)
 })
 
@@ -15,9 +17,12 @@ test_that("regr.abess predict", {
   learner = lrn("regr.abess")
   fun = abess:::predict.abess # nolint
   exclude = c(
-    "type"
+    "support.size", # handled by mlr3
+    "type", # regr needn't type
+    "newx", # handled by mlr3
+    "object" # handled by mlr3
   )
 
-  paramtest = run_paramtest(learner, fun, exclude)
+  paramtest = run_paramtest(learner, fun, exclude, tag = 'perdict')
   expect_paramtest(paramtest)
 })
