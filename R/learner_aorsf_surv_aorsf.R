4b6e0a2a905ddccc412ff34812bd25994abfe391
bcjaeger <byron.jaeger@gmail.com>
2022-08-29 13:32:24 -0400

    getting params right

--- R/learner_aorsf_surv_aorsf.R
+++ R/learner_aorsf_surv_aorsf.R
@@ -37,32 +37,116 @@ delayedAssign(
             #' Creates a new instance of this [R6][R6::R6Class] class.
             initialize = function() {
               ps = ps(
-                n_tree = p_int(default = 500L, lower = 1L, tags = 'train'),
-                n_split = p_int(default = 5L, lower = 1L, tags = 'train'),
-                n_retry = p_int(default = 3L, lower = 0L, tags = 'train'),
+
+                n_tree = p_int(default = 500L,
+                               lower = 1L,
+                               tags = 'train'),
+
+                n_split = p_int(default = 5L,
+                                lower = 1L,
+                                tags = 'train'),
+
+                n_retry = p_int(default = 3L,
+                                lower = 0L,
+                                tags = 'train'),
+
                 mtry = p_int(lower = 1L,
                              tags = 'train'),
-                mtry_ratio = p_dbl(0, 1, tags = "train"),
-                leaf_min_events = p_int(default = 1L, lower = 1L, tags = 'train'),
-                leaf_min_obs = p_int(default = 5L, lower = 1L, tags = 'train'),
-                split_min_events = p_int(default = 5L, lower = 1L, tags = 'train'),
-                split_min_obs = p_int(default = 10, lower = 1L, tags = 'train'),
-                split_min_stat = p_dbl(default = 3.841459, lower = 0, tags = 'train'),
-                oobag_pred_type = p_fct(levels = c("none", "surv", "risk", "chf"),
-                                        default = "none",
+
+                control_type = p_fct(levels = c("fast", "cph", "net"),
+                                     default = "fast",
+                                     tags = "train"),
+
+                control_fast_do_scale = p_lgl(default = TRUE,
+                                              tags = "train"),
+
+                control_fast_method = p_fct(levels = c("efron", "breslow"),
+                                            default = "breslow",
+                                            tags = "train"),
+
+                control_cph_method = p_fct(levels = c("efron", "breslow"),
+                                           default = "breslow",
+                                           tags = "train"),
+
+                control_cph_eps = p_dbl(default = 1e-9,
+                                        lower = 0,
+                                        tags = "train"),
+
+                control_cph_iter_max = p_int(default = 20L,
+                                             lower = 1,
+                                             tags = "train"),
+
+                control_net_alpha = p_dbl(default = 0.5,
+                                          tags = "train"),
+
+                control_net_df_target = p_int(lower = 1L,
+                                              tags = 'train'),
+
+                mtry_ratio = p_dbl(lower = 0,
+                                   upper = 1,
+                                   tags = "train"),
+
+                leaf_min_events = p_int(default = 1L,
+                                        lower = 1L,
                                         tags = 'train'),
-                oobag_pred_horizon = p_uty(tags = "train")
+
+                leaf_min_obs = p_int(default = 5L,
+                                     lower = 1L,
+                                     tags = 'train'),
+
+                split_min_events = p_int(default = 5L,
+                                         lower = 1L,
+                                         tags = 'train'),
+
+                split_min_obs = p_int(default = 10,
+                                      lower = 1L,
+                                      tags = 'train'),
+
+                split_min_stat = p_dbl(default = 3.841459,
+                                       lower = 0,
+                                       tags = 'train'),
+
+                oobag_pred_type = p_fct(levels = c("none",
+                                                   "surv",
+                                                   "risk",
+                                                   "chf"),
+                                        default = "surv",
+                                        tags = 'train'),
+
+                importance = p_fct(levels = c("none",
+                                              "anova",
+                                              "negate",
+                                              "permute"),
+                                   default = "anova",
+                                   tags = 'train'),
+
+                oobag_pred_horizon = p_uty(tags = "train"),
+
+                oobag_eval_every = p_int(lower = 1,
+                                         tags = "train"),
+
+                attach_data = p_lgl(default = TRUE, tags = "train")
+
               )
 
               super$initialize(
+
                 id = "surv.aorsf",
+
                 packages = c("mlr3extralearners", "aorsf", "pracma"),
+
                 feature_types = c("integer", "numeric", "factor", "ordered"),
+
                 predict_types = c("crank", "distr"),
+
                 param_set = ps,
+
                 properties = c("oob_error"),
+
                 man = "mlr3extralearners::mlr_learners_surv.aorsf",
+
                 label = "Oblique Random Forest"
+
               )
             },
 
@@ -71,28 +155,102 @@ delayedAssign(
             #' @return `numeric()`.
             oob_error = function() {
 
-              nrows <- nrow(self$eval_oobag$stat_values)
-              1 - self$eval_oobag$stat_values[nrows, 1L]
+              nrows <- nrow(self$model$eval_oobag$stat_values)
+              1 - self$model$eval_oobag$stat_values[nrows, 1L]
+
+            },
+
+            #' @description
+            #' The importance scores are extracted from the model.
+            #' @return Named `numeric()`.
+            importance = function() {
+
+              if (is.null(self$model)) {
+                stopf("No model stored")
+              }
+
+              aorsf::orsf_vi(self$model, group_factors = TRUE)
 
             }
+
           ),
 
           private = list(
             .train = function(task) {
 
+              # start w/defaults
+              ps <- self$param_set$default
+
+              # pull in user specified values
               pv = self$param_set$get_values(tags = "train")
-              pv = convert_ratio(pv, "mtry", "mtry_ratio", length(task$feature_names))
-              pv = c(pv, list(weights = task$weights$weight))
+
+              # if user specified a value, put it into the param set
+              for( i in names(ps) ){
+                if(!is.null(pv[[i]])) ps[[i]] <- pv[[i]]
+              }
+
+              for( i in names(pv) ){
+                if( !(i %in% names(ps)) ) ps[[i]] <- pv[[i]]
+              }
+
+              ps = convert_ratio(ps,
+                                 target = "mtry",
+                                 ratio = "mtry_ratio",
+                                 length(task$feature_names))
+
+              ps = c(ps, list(weights = task$weights$weight))
+
+              switch(
+
+                ps$control_type,
+
+                "fast" = {
+                  control <- aorsf::orsf_control_fast(
+                    method = ps$control_fast_method,
+                    do_scale = ps$control_fast_do_scale
+                  )
+                },
+
+                "cph" = {
+                  control <- aorsf::orsf_control_cph(
+                    method = ps$control_cph_method,
+                    eps = ps$control_cph_eps,
+                    iter_max = ps$control_cph_iter_max
+                  )
+                },
+
+                "net" = {
+                  control <- aorsf::orsf_control_net(
+                    alpha = ps$control_net_alpha,
+                    df_target = ps$control_net_df_target
+                  )
+                }
+
+              )
+
+              ps$control_type <- NULL
+              ps$control_fast_do_scale <- NULL
+              ps$control_fast_method <- NULL
+              ps$control_cph_method <- NULL
+              ps$control_cph_eps <- NULL
+              ps$control_cph_iter_max <- NULL
+              ps$control_net_alpha <- NULL
+              ps$control_net_df_target <- NULL
 
               tn = task$target_names
-              lhs = sprintf("Surv(%s, %s)", tn[1L], tn[2L])
-              f = formulate(lhs, task$feature_names, env = getNamespace("survival"))
+
+              lhs = paste(tn, collapse = ' + ')
+
+              f = formulate(lhs = lhs,
+                            rhs = task$feature_names)
 
               invoke(
                 aorsf::orsf,
                 data = task$data(),
                 formula = f,
-                .args = pv
+                control = control,
+                no_fit = FALSE,
+                .args = ps
               )
 
             },
---
+++ tests/testthat/test_paramtest_aorsf_surv_aorsf.R
@@ -0,0 +1,42 @@
+
+test_that("paramtest surv.aorsf train", {
+  learner = lrn("surv.aorsf")
+  fun_list = list(aorsf::orsf)
+  exclude = c(
+    "control",# handled internally
+    "weights",# handled internally
+    "oobag_fun", # handled internally
+    "data", # handled internally
+    "formula", # handled internally
+    "control", # handled internally
+    "oobag_fun", # handled internally
+    "tree_seeds", # handled internally
+    "new_data", # handled internally
+    "object", # handled internally
+    "mtry_ratio", # manually added as an alternative to "mtry"
+    "no_fit", # handled internally
+    "control_type", # handled internally
+    "control_fast_do_scale", # handled internally
+    "control_fast_method", # handled internally
+    "control_cph_method", # handled internally
+    "control_cph_eps", # handled internally
+    "control_cph_iter_max", # handled internally
+    "control_net_alpha", # handled internally
+    "control_net_df_target" # handled internally
+  )
+  paramtest = run_paramtest(learner, fun_list, exclude, tag = "train")
+  expect_paramtest(paramtest)
+})
+
+test_that("paramtest surv.aorsf predict", {
+  learner = lrn("surv.aorsf")
+  fun_list = list(aorsf:::predict.orsf_fit)
+  exclude = c(
+    "object", # handled internally
+    "new_data", # handled internally
+    "pred_horizon", # all times required for distr
+    "pred_type" # handled internally
+  )
+  paramtest = run_paramtest(learner, fun_list, exclude, tag = "predict")
+  expect_paramtest(paramtest)
+})
