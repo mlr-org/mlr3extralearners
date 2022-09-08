b439d8bf12751563608ba192bd2b6ebcab4400a8
Sebastian Fischer <sebf.fischer@gmail.com>
2022-09-08 17:25:30 +0200

    feature: add rsm learner

--- R/learner_rsm_regr_rsm.R
+++ R/learner_rsm_regr_rsm.R
@@ -6,20 +6,17 @@
 #' Fit a linear model with a response-surface component.
 #' Calls [rsm::rsm()] from FIXME: \CRANpkg{rsm}.
 #'
-#' @section Custom mlr3 defaults:
-#' FIXME: DEVIATIONS FROM UPSTREAM PARAMETERS. DELETE IF NOT APPLICABLE.
-#'
-#' @section Custom mlr3 defaults:
-#' FIXME: DEVIATIONS FROM UPSTREAM DEFAULTS. DELETE IF NOT APPLICABLE.
-#'
-#' @section Installation:
-#' FIXME: CUSTOM INSTALLATION INSTRUCTIONS. DELETE IF NOT APPLICABLE.
+#' @section Custom mlr3 parameters:
+#' - `modelfun`: This parameter controls how the formula for [rsm::rsm()] is created. Possible values are:
+#'   - `"FO"` - first order
+#'   - `"TWI"` - wo-way interactions, this is with 1st oder terms
+#'   - `"SO"` - full second order
 #'
 #' @templateVar id regr.rsm
 #' @template learner
 #'
 #' @references
-#' `r format_bib(FIXME: ONE OR MORE REFERENCES FROM bibentries.R)`
+#' `r format_bib("lenth2010response")`
 #'
 #' @template seealso_learner
 #' @template example
@@ -30,11 +27,10 @@ LearnerRegrRSM = R6Class("LearnerRegrRSM",
     #' @description
     #' Creates a new instance of this [R6][R6::R6Class] class.
     initialize = function() {
-      # FIXME: MANUALLY ADD PARAMETERS BELOW AND THEN DELETE THIS LINE
-      param_set = ps()
-
-      # FIXME: MANUALLY UPDATE PARAM VALUES BELOW IF APPLICABLE THEN DELETE THIS LINE.
-      param_set$values = list()
+      param_set = ps(
+        modelfun   = p_fct(default = "FO", levels = c("FO", "TWI", "SO"), tags = c("train", "required"))
+      )
+      param_set$values = list(modelfun = "FO")
 
       super$initialize(
         id = "regr.rsm",
@@ -42,7 +38,7 @@ LearnerRegrRSM = R6Class("LearnerRegrRSM",
         feature_types = c("integer", "numeric", "factor", "ordered"),
         predict_types = c("response"),
         param_set = param_set,
-        properties = c("weights"),
+        properties = character(0),
         man = "mlr3extralearners::mlr_learners_regr.rsm",
         label = "Response Surface Model"
       )
@@ -50,28 +46,25 @@ LearnerRegrRSM = R6Class("LearnerRegrRSM",
   ),
   private = list(
     .train = function(task) {
+      browser()
       # get parameters for training
       pars = self$param_set$get_values(tags = "train")
+      vs = paste(task$feature_names, collapse = ",", sep = " ")
+      g = function(x) paste(x, "(", vs, ")", sep = "")
 
-      # FIXME: IF LEARNER DOES NOT HAVE 'weights' PROPERTY THEN DELETE THESE LINES.
-      if ("weights" %in% task$properties) {
-        # Add weights to learner
-      }
+      mf = switch(pars$modelfun,
+        FO = g("FO"),
+        TWI = paste(g("TWI"), "+", g("FO"), sep = " "),
+        SO = g("SO"),
+        stop("Unknown modelfun: ", mf)
+      )
+      formula = as.formula(paste(task$target_names, "~", mf, sep = " "))
 
-      # FIXME: CREATE OBJECTS FOR THE TRAIN CALL
-      # AT LEAST "data" AND "formula" ARE REQUIRED
-      formula = task$formula()
       data = task$data()
-
-      # FIXME: HERE IS SPACE FOR SOME CUSTOM ADJUSTMENTS BEFORE PROCEEDING TO THE
-      # TRAIN CALL. CHECK OTHER LEARNERS FOR WHAT CAN BE DONE HERE
-      # USE THE mlr3misc::invoke FUNCTION (IT'S SIMILAR TO do.call())
-
       invoke(
         rsm::rsm,
         formula = formula,
-        data = data,
-        .args = pars
+        data = data
       )
     },
     .predict = function(task) {
@@ -81,13 +74,10 @@ LearnerRegrRSM = R6Class("LearnerRegrRSM",
       # get newdata and ensure same ordering in train and predict
       newdata = ordered_features(task, self)
 
-      # Calculate predictions for the selected predict type.
-      type = self$predict_type
+      pred = invoke(predict, self$model, newdata = newdata, .args = pars)
+      names(pred) = NULL
 
-      pred = invoke(predict, self$model, newdata = newdata, type = type, .args = pars)
-
-      # FIXME: ADD PREDICTIONS TO LIST BELOW
-      list()
+      list(response = pred)
     }
   )
 )
--- tests/testthat/test_paramtest_rsm_regr_rsm.R
+++ tests/testthat/test_paramtest_rsm_regr_rsm.R
@@ -3,24 +3,27 @@ test_that("regr.rsm train", {
   fun = rsm::rsm
   exclude = c(
     "object", # handled internally
-    "data" # handled internally
+    "data", # handled internally
+    "modelfun", # custom parameter
+    "formula"
   )
 
   # note that you can also pass a list of functions in case $.train calls more than one
   # function, e.g. for control arguments
-  paramtest = run_paramtest(learner, fun, exclude)
+  paramtest = run_paramtest(learner, fun, exclude, tag = "train")
   expect_paramtest(paramtest)
 })
 
 test_that("regr.rsm predict", {
   learner = lrn("regr.rsm")
-  fun = rsm::predict # nolint
-    exclude = c(
-      "object", # handled internally
-      "data", # handled internally
-      "newdata" # handled internally
-    )
+  fun = predict
+  exclude = c(
+    "object", # handled internally
+    "data", # handled internally
+    "newdata" # handled internally
 
-  paramtest = run_paramtest(learner, fun, exclude)
+  )
+
+  paramtest = run_paramtest(learner, fun, exclude, tag = "predict")
   expect_paramtest(paramtest)
 })
