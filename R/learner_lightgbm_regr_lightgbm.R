1db7a1c2ccd5f6d5b39bac99c1e8c508f941233e
Sebastian Fischer <sebf.fischer@gmail.com>
2023-01-10 17:36:58 +0100

    fix: mising lightgbm parameter

--- R/learner_lightgbm_regr_lightgbm.R
+++ R/learner_lightgbm_regr_lightgbm.R
@@ -72,6 +72,7 @@ LearnerRegrLightGBM = R6Class("LearnerRegrLightGBM",
         device_type = p_fct(default = "cpu", levels = c("cpu", "gpu"), tags = "train"),
         seed = p_int(tags = "train"),
         deterministic = p_lgl(default = FALSE, tags = "train"),
+        data_sample_strategy = p_fct(default = "bagging", levels =  c("bagging", "goss")),
 
         # Learning control parameters
         force_col_wise = p_lgl(default = FALSE, tags = "train"),
