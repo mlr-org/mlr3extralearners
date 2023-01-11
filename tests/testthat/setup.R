ab286ee5874e47b89bb135da917f885fd95cfa51
Sebastian Fischer <sebf.fischer@gmail.com>
2023-01-11 15:31:12 +0100

    fix: setup and teardown for tests

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -6,8 +6,4 @@ old_opts = options(
 
 # https://github.com/HenrikBengtsson/Wishlist-for-R/issues/88
 old_opts = lapply(old_opts, function(x) if (is.null(x)) FALSE else x)
-
-old_threshold = lg$threshold
-old_plan = future::plan()
-lg$set_threshold("warn")
 future::plan("sequential")
