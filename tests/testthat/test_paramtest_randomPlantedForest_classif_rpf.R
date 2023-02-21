a3c8322ab6422a1e47ad9732c0cc4c7933a78738
Lukas Burk <lukas@quantenbrot.de>
2023-02-21 01:33:50 +0100

    Tweak paramtests

--- tests/testthat/test_paramtest_randomPlantedForest_classif_rpf.R
+++ tests/testthat/test_paramtest_randomPlantedForest_classif_rpf.R
@@ -1,28 +1,28 @@
 test_that("classif.rpf train", {
   learner = lrn("classif.rpf")
-  fun = randomPlantedForest::rpf
+  fun.list = list(randomPlantedForest:::rpf.data.frame)
   exclude = c(
-    "x", # handled internally
-    "data" # handled internally
+    "x", "y", # handled internally
+    "max_interaction_ratio",
+    "max_interaction_limit"
   )
 
   # note that you can also pass a list of functions in case $.train calls more than one
   # function, e.g. for control arguments
-  paramtest = run_paramtest(learner, fun, exclude)
+  paramtest = run_paramtest(learner, fun.list, exclude, tag = "train")
   expect_paramtest(paramtest)
 })
 
 test_that("classif.rpf predict", {
   learner = lrn("classif.rpf")
-  fun = predict # nolint
+  fun.list = list(predict) # nolint
   exclude = c(
     "object", # handled internally
     "data", # handled internally
     "new_data", # handled internally
-    "type", # Not sure but handled by predict_type?
-    "components" # FIXME: NYI
+    "type" # Not sure but handled by predict_type?
   )
 
-  paramtest = run_paramtest(learner, fun, exclude, tag = "predict")
+  paramtest = run_paramtest(learner, fun.list, exclude, tag = "predict")
   expect_paramtest(paramtest)
 })
--- tests/testthat/test_paramtest_randomPlantedForest_regr_rpf.R
+++ tests/testthat/test_paramtest_randomPlantedForest_regr_rpf.R
@@ -1,31 +1,31 @@
 test_that("regr.rpf train", {
   learner = lrn("regr.rpf")
-  fun = randomPlantedForest::rpf
+  fun.list = list(randomPlantedForest:::rpf.data.frame)
   exclude = c(
-    "x", # handled internally
-    "data", # handled internally
+    "x", "y", # handled internally
     "loss", # Not applicable for regression learner
     "delta", # Not applicable for regression learner
-    "epsilon" # Not applicable for regression learner
+    "epsilon", # Not applicable for regression learner
+    "max_interaction_ratio",
+    "max_interaction_limit"
   )
 
   # note that you can also pass a list of functions in case $.train calls more than one
   # function, e.g. for control arguments
-  paramtest = run_paramtest(learner, fun, exclude)
+  paramtest = run_paramtest(learner, fun.list, exclude, tag = "train")
   expect_paramtest(paramtest)
 })
 
 test_that("regr.rpf predict", {
   learner = lrn("regr.rpf")
-  fun = predict # nolint
+  fun.list = list(predict) # nolint
   exclude = c(
     "object", # handled internally
     "data", # handled internally
     "new_data", # handled internally
-    "type", # Not sure but handled by predict_type?
-    "components" # FIXME: NYI
+    "type" # Not sure but handled by predict_type?
   )
 
-  paramtest = run_paramtest(learner, fun, exclude, tag = "predict")
+  paramtest = run_paramtest(learner, fun.list, exclude, tag = "predict")
   expect_paramtest(paramtest)
 })
