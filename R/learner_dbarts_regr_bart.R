cf1ba25a925f78b0dd572724cd6951333942d024
Sebastian Fischer <sebf.fischer@gmail.com>
2023-02-27 17:33:01 +0100

    feat: add missing parameters
    Due to changes in the package "dbarts" and "survivalmodels"
    the parameter tests started to fail.
    This adds the missing parameters

--- R/learner_dbarts_regr_bart.R
+++ R/learner_dbarts_regr_bart.R
@@ -59,7 +59,9 @@ LearnerRegrBart = R6Class("LearnerRegrBart",
         sampleronly = p_lgl(default = FALSE, tags = "train"),
         seed = p_int(default = NA_integer_, tags = "train", special_vals = list(NA_integer_)),
         proposalprobs = p_uty(default = NULL, tags = "train"),
-        splitprobs = p_uty(default = NULL, tags = "train")
+        splitprobs = p_uty(default = NULL, tags = "train"),
+        weights = p_uty(tags = "train"),
+        keepsampler = p_lgl(default = NO_DEF, tags = "train")
       )
 
       ps$values = list(keeptrees = TRUE)
