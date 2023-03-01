53a8a62dbdfea52b619d5467ce081299e71bc3f7
Sebastian Fischer <sebf.fischer@gmail.com>
2023-03-01 10:11:55 +0100

    tests(abess): disable failing test for abess classif learner
    This was not a issue previously and might have been introduced
    in abess 0.4.7
    
    https://github.com/abess-team/abess/issues/493

--- tests/testthat/test_abess_classif_abess.R
+++ tests/testthat/test_abess_classif_abess.R
@@ -1,7 +1,8 @@
 test_that("autotest", {
   learner = lrn("classif.abess")
   expect_learner(learner)
-  # note that you can skip tests using the exclude argument
-  result = run_autotest(learner, check_replicable = FALSE)
+  # See https://github.com/abess-team/abess/issues/493 for the exclusion argument
+  result = run_autotest(learner, check_replicable = FALSE, exclude = "feat_single")
+
   expect_true(result, info = result$error)
 })
