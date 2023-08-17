8fa59a2dd70a54a26cbce06d46342c12282f471d
john <bblodfon@gmail.com>
2023-08-17 13:18:34 +0200

    add BART survival learner

---
+++ tests/testthat/test_paramtest_BART_surv_bart.R
@@ -0,0 +1,36 @@
+test_that("paramtest surv.bart train", {
+  learner = lrn("surv.bart")
+  fun_list = list(BART::mc.surv.bart)
+  exclude = c(
+    "x.train", # handled internally
+    "y.train", # handled internally
+    "times", # handled internally
+    "delta", # handled internally
+    "x.test", # not used
+    "xinfo",  # not used
+    "tau.num", # not used, automatically calculated from `type` and `ntype`
+    "id" # only for `surv.bart`
+  )
+
+  paramtest = run_paramtest(learner, fun_list, exclude, tag = "train")
+  expect_paramtest(paramtest)
+})
+
+test_that("paramset surv.bart predict", {
+  learner = lrn("surv.bart")
+  fun_list = list(BART:::predict.survbart, BART::surv.pre.bart) # nolint
+  exclude = c(
+    "object", # handled internally
+    "newdata", # handled internally
+    "openmp", # handled internally
+    "times", # handled internally, used in `surv.pre.bart`
+    "delta", # handled internally, used in `surv.pre.bart`
+    "x.train", # handled internally, used in `surv.pre.bart`
+    "x.test", # handled internally, used in `surv.pre.bart`
+    "nice", # handled internally
+    "quiet" # added to suppress print messages
+  )
+
+  paramtest = run_paramtest(learner, fun_list, exclude, tag = "predict")
+  expect_paramtest(paramtest)
+})
