3088ae8227040030687f7be13a8ee107260280cd
Zhaolin Xu <96219790+Brandonxzl@users.noreply.github.com>
2023-10-18 14:38:36 +0200

    fix: line length

--- man-roxygen/example.R
+++ man-roxygen/example.R
@@ -1,8 +1,15 @@
 <%
-lrn = mlr3::lrn(id)
-pkgs = setdiff(lrn$packages, c("mlr3", "mlr3learners"))
+pkgs = setdiff(mlr3::lrn(id)$packages, c("mlr3", "mlr3learners"))
+if ("LearnerClassif" %in% class(lrn(id))) {
+  task_id = "sonar"
+} else if ("LearnerRegr" %in% class(lrn(id))) {
+  task_id = "mtcars"
+} else if ("LearnerSurv" %in% class(lrn(id))) {
+  task_id = "grace"
+} else if ("LearnerDens" %in% class(lrn(id))) {
+  task_id = "faithful"
+}
 %>
-#' <% if ("LearnerClassif" %in% class(lrn(id))) {task_id = "sonar"} else if ("LearnerRegr" %in% class(lrn(id))) {task_id = "mtcars"} else if ("LearnerSurv" %in% class(lrn(id))) {task_id = "grace"} else if ("LearnerDens" %in% class(lrn(id))) {task_id = "faithful"}%>
 #'
 #' @examples
 #' if (<%= paste0("requireNamespace(\"", pkgs, "\", quietly = TRUE)", collapse = " && ") %>) {
