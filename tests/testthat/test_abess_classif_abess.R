4915ac1169dfca85294898a3be5c6ba7c6c857b2
Sebastian Fischer <sebf.fischer@gmail.com>
2023-10-20 10:22:08 +0200

    test: re-enable abess tests (#309)
    the abess team fixed the bug in a recent release
    
    fixes #271

--- tests/testthat/test_abess_classif_abess.R
+++ tests/testthat/test_abess_classif_abess.R
@@ -1,8 +1,7 @@
 test_that("autotest", {
   learner = lrn("classif.abess")
   expect_learner(learner)
-  # See https://github.com/abess-team/abess/issues/493 for the exclusion argument
-  result = run_autotest(learner, check_replicable = FALSE, exclude = "feat_single")
+  result = run_autotest(learner, check_replicable = FALSE)
 
   expect_true(result, info = result$error)
 })
