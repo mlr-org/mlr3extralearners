be85fda3e333e491e7303db421cf4ffa024040ac
damirpolat <damirpolat@protonmail.com>
2023-11-20 14:56:08 -0700

    add rweka SGD regressor

---
+++ R/learner_RWeka_regr_SGD.R
@@ -0,0 +1,106 @@
+#' @title Regression Stochastic Gradient Descent For Learning Various Linear Models
+#' @author damirpolat
+#' @name mlr_learners_regr.SGD
+#'
+#' @description
+#' Stochastic Gradient Descent for learning various linear models
+#' Calls [RWeka::make_Weka_classifier('weka/classifiers/functions/SGD')] \CRANpkg{RWeka}'.
+#'
+#' @section Initial parameter values:
+#' - `F`:
+#'    - Has only 3 out of 5 original loss functions: 2 = squared loss (regression), 
+#'      3 = epsilon insensitive loss (regression) and 4 = Huber loss (regression) with 2 (squared loss) being the new default
+#'    - Reason for change: this learner should only contain loss functions appropriate for regression tasks
+#'
+#' @section Custom mlr3 parameters:
+#' - `output_debug_info`:
+#'   - original id: output-debug-info
+#'
+#' - `do_not_check_capabilities`:
+#'   - original id: do-not-check-capabilities
+#'
+#' - `num_decimal_places`:
+#'   - original id: num-decimal-places
+#'
+#' - `batch_size`:
+#'   - original id: batch-size
+#'
+#' - Reason for change: This learner contains changed ids of the following control arguments
+#' since their ids contain irregular pattern
+#' 
+#'
+#' @templateVar id regr.SGD
+#' @template learner
+#'
+#' @template seealso_learner
+#' @template example
+#' @export
+LearnerRegrSGD = R6Class("LearnerRegrSGD",
+  inherit = LearnerRegr,
+  public = list(
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    initialize = function() {
+      param_set = ps(
+        subset = p_uty(tags = "train"),
+        na.action = p_uty(tags = "train"),
+        F = p_fct(default = "2", levels = c("2", "3", "4"), tags = "train"),
+        L = p_dbl(default = 0.01, tags = "train"),
+        R = p_dbl(default = 0.0001, tags = "train"),
+        E = p_int(default = 500L, tags = "train"),
+        C = p_dbl(default = 1e-3, tags = "train"),
+        N = p_lgl(default = FALSE, tags = "train"),
+        M = p_lgl(default = FALSE, tags = "train"),
+        S = p_int(default = 1L, tags = "train"),
+        output_debug_info = p_lgl(default = FALSE, tags = "train"),
+        do_not_check_capabilities = p_lgl(default = FALSE,
+                                          tags = "train"),
+        num_decimal_places = p_int(default = 2L, lower = 1L,
+                                   tags = "train"),
+        batch_size = p_int(default = 100L, lower = 1L, tags = "train"),
+        options = p_uty(default = NULL, tags = "train")
+      )
+
+      super$initialize(
+        id = "regr.SGD",
+        packages = "RWeka",
+        feature_types = c("logical", "integer", "numeric", "factor", "ordered"),
+        predict_types = c("response"),
+        param_set = param_set,
+        properties = character(0L),
+        man = "mlr3extralearners::mlr_learners_regr.SGD",
+        label = "Stochastic Gradient Descent For Learning Various Linear Models"
+      )
+    }
+  ),
+  
+  private = list(
+    .train = function(task) {
+      weka_learner = RWeka::make_Weka_classifier('weka/classifiers/functions/SGD')
+      
+      pars = self$param_set$get_values(tags = "train")
+      ctrl_arg_names = weka_control_args(weka_learner)
+      arg_names = setdiff(names(pars), ctrl_arg_names)
+      ctrl = pars[which(names(pars) %in% ctrl_arg_names)]
+      pars = pars[which(names(pars) %nin% ctrl_arg_names)]
+      
+      if (length(ctrl) > 0L) {
+        names(ctrl) = gsub("_", replacement = "-", x = names(ctrl))
+        ctrl = invoke(RWeka::Weka_control, .args = ctrl)
+      }
+      
+      formula = task$formula()
+      data = task$data()
+      invoke(weka_learner, formula = formula, data = data, control = ctrl)
+    },
+    
+    .predict = function(task) {
+      newdata = ordered_features(task, self)
+      pars = self$param_set$get_values(tags = "predict")
+      response = invoke(predict, self$model, newdata = newdata, .args = pars)
+      list(response = response)
+    }
+  )
+)
+
+.extralrns_dict$add("regr.SGD", LearnerRegrSGD)
---
+++ tests/testthat/test_paramtest_RWeka_regr_SGD.R
@@ -0,0 +1,25 @@
+test_that("regr.SGD train", {
+  learner = lrn("regr.SGD")
+  fun = RWeka::make_Weka_classifier('weka/classifiers/functions/SGD')
+  exclude = weka_control_args(fun)
+  # formula and data are handled via mlr3
+  # mlr3 does not have the `control` argument because the parameters can be specified directly
+  exclude = c("formula", "data", "control", exclude)
+  paramtest = run_paramtest(learner, fun, exclude, tag = "train")
+  expect_paramtest(paramtest)
+  control_args = weka_control_args(fun)
+  expect_true(all(control_args %in% learner$param_set$ids()))
+})
+
+test_that("regr.SGD predict", {
+  # Here we test that the learner implements those arguments that are passed via the
+  # control argument to RWeka::make_Weka_classifier('weka/classifiers/functions/SGD')
+  learner = lrn("regr.SGD")
+  exclude = c( # all handled by mlr3
+    "object",
+    "newdata",
+    "type"
+  )
+  paramtest = run_paramtest(learner, RWeka:::predict.Weka_classifier, exclude, tag = "predict") # nolint
+  expect_paramtest(paramtest)
+})
