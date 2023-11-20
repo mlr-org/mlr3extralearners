9fd3e15012b3e83d54a34902c3b662761353320f
damirpolat <damirpolat@protonmail.com>
2023-11-19 22:17:51 -0700

    add rweka kstar regr

---
+++ tests/testthat/test_RWeka_regr_KStar.R
@@ -0,0 +1,8 @@
+skip_on_os("windows")
+
+test_that("autotest", {
+  learner = lrn("regr.KStar")
+  expect_learner(learner)
+  result = run_autotest(learner)
+  expect_true(result, info = result$error)
+})
