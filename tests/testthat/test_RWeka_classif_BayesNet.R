ebbd5fc266e52a9cd3cdb5a71854d9d2d32ccc75
damirpolat <damirpolat@protonmail.com>
2023-11-20 18:24:13 -0700

    exclude missings_each_row task from BayesNet classifier

--- tests/testthat/test_RWeka_classif_BayesNet.R
+++ tests/testthat/test_RWeka_classif_BayesNet.R
@@ -3,6 +3,6 @@ skip_on_os("windows")
 test_that("autotest", {
   learner = lrn("classif.BayesNet")
   expect_learner(learner)
-  result = run_autotest(learner)
+  result = run_autotest(learner, exclude = "(missings_each_row)")
   expect_true(result, info = result$error)
 })
