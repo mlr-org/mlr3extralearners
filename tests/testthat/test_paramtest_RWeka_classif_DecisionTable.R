1c5ecec5712b296d72e26a4452ac0fee43defbc7
damirpolat <damirpolat@protonmail.com>
2023-11-20 21:48:22 -0700

    exclude SBF from tests for DecisionTable

--- tests/testthat/test_paramtest_RWeka_classif_DecisionTable.R
+++ tests/testthat/test_paramtest_RWeka_classif_DecisionTable.R
@@ -4,11 +4,11 @@ test_that("classif.DecisionTable train", {
   exclude = weka_control_args(fun)
   # formula and data are handled via mlr3
   # mlr3 does not have the `control` argument because the parameters can be specified directly
-  exclude = c("formula", "data", "control", exclude)
+  exclude = c("formula", "data", "control", "SBF", exclude)
   paramtest = run_paramtest(learner, fun, exclude, tag = "train")
   expect_paramtest(paramtest)
   control_args = weka_control_args(fun)
-  expect_true(all(control_args %in% learner$param_set$ids()))
+  expect_true(all(control_args %in% setdiff(learner$param_set$ids(), "SBF")))
 })
 
 test_that("classif.DecisionTable predict", {
