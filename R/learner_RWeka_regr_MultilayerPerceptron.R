14d7ae5497fa4767001f3b61e67e35bac9c08473
damirpolat <damirpolat@protonmail.com>
2023-11-26 16:59:31 -0700

    add multilayer perceptron regressor from WEKA

---
+++ R/learner_RWeka_regr_MultilayerPerceptron.R
@@ -0,0 +1,105 @@
+#' @title Regression Regressor That Uses Backpropagation To Learn A Multi-layer Perceptron
+#' @author damirpolat
+#' @name mlr_learners_regr.MultilayerPerceptron
+#'
+#' @description
+#' Regressor that uses backpropagation to learn a multi-layer perceptron
+#' Calls [RWeka::make_Weka_classifier('weka/classifiers/functions/MultilayerPerceptron')] from \CRANpkg{RWeka}.
+#'
+#' @section Custom mlr3 parameters:
+#' - `output_debug_info`:
+#'   - original id: output-debug-info
+#'
+#' - `do_not_check_capabilities`:
+#'   - original id: do-not-check-capabilities
+#'
+#' - `num_decimal_places`:
+#'   - original id: num-decimal-places
+#'
+#' - `batch_size`:
+#'   - original id: batch-size
+#'
+#' - Reason for change: This learner contains changed ids of the following control arguments
+#' since their ids contain irregular pattern
+#' 
+#'
+#' @templateVar id regr.MultilayerPerceptron
+#' @template learner
+#'
+#' @template seealso_learner
+#' @template example
+#' @export
+LearnerRegrMultilayerPerceptron = R6Class("LearnerRegrMultilayerPerceptron",
+  inherit = LearnerRegr,
+  public = list(
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    initialize = function() {
+      param_set = ps(
+        subset = p_uty(tags = "train"),
+        na.action = p_uty(tags = "train"),
+        L = p_dbl(default = 0.3, lower = 0L, upper = 1L, tags = "train"),
+        M = p_dbl(default = 0.2, lower = 0L, upper = 1L, tags = "train"),
+        N = p_int(default = 500L, lower = 1L, tags = "train"),
+        V = p_dbl(default = 0L, lower = 0L, upper = 100L, tags = "train"),
+        S = p_int(default = 0L, lower = 0L, tags = "train"),
+        E = p_int(default = 20L, lower = 1L, tags = "train"),
+        A = p_lgl(default = FALSE, tags = "train"),
+        B = p_lgl(default = FALSE, tags = "train"),
+        H = p_uty(tags = "train"),
+        C = p_dbl(default = FALSE, tags = "train"),
+        I = p_dbl(default = FALSE, tags = "train"),
+        R = p_dbl(default = FALSE, tags = "train"),
+        D = p_dbl(default = FALSE, tags = "train"),
+        output_debug_info = p_lgl(default = FALSE, tags = "train"),
+        do_not_check_capabilities = p_lgl(default = FALSE,
+                                          tags = "train"),
+        num_decimal_places = p_int(default = 2L, lower = 1L,
+                                   tags = "train"),
+        batch_size = p_int(default = 100L, lower = 1L, tags = "train"),
+        options = p_uty(default = NULL, tags = "train")
+      )
+
+      super$initialize(
+        id = "regr.MultilayerPerceptron",
+        packages = "RWeka",
+        feature_types = c("logical", "integer", "numeric", "factor", "ordered"),
+        predict_types = c("response"),
+        param_set = param_set,
+        properties = c("missings"),
+        man = "mlr3extralearners::mlr_learners_regr.MultilayerPerceptron",
+        label = "Regressor That Uses Backpropagation To Learn A Multi-layer Perceptron"
+      )
+    }
+  ),
+  
+  private = list(
+    .train = function(task) {
+      weka_learner = RWeka::make_Weka_classifier('weka/classifiers/functions/MultilayerPerceptron')
+      
+      pars = self$param_set$get_values(tags = "train")
+      ctrl_arg_names = weka_control_args(weka_learner)
+      arg_names = setdiff(names(pars), ctrl_arg_names)
+      ctrl = pars[which(names(pars) %in% ctrl_arg_names)]
+      pars = pars[which(names(pars) %nin% ctrl_arg_names)]
+      
+      if (length(ctrl) > 0L) {
+        names(ctrl) = gsub("_", replacement = "-", x = names(ctrl))
+        ctrl = invoke(RWeka::Weka_control, .args = ctrl)
+      }
+      
+      formula = task$formula()
+      data = task$data()
+      invoke(weka_learner, formula = formula, data = data, control = ctrl)
+    },
+    
+    .predict = function(task) {
+      newdata = ordered_features(task, self)
+      pars = self$param_set$get_values(tags = "predict")
+      response = invoke(predict, self$model, newdata = newdata, .args = pars)
+      list(response = response)
+    }
+  )
+)
+
+.extralrns_dict$add("regr.MultilayerPerceptron", LearnerRegrMultilayerPerceptron)
