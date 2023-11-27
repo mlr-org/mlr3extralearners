85f56f8d18e86a297d8f56dab6fed989103df6b5
damirpolat <damirpolat@protonmail.com>
2023-11-26 21:55:52 -0700

    add simple logistic classifier from weka

---
+++ R/learner_RWeka_classif_SimpleLogistic.R
@@ -0,0 +1,112 @@
+#' @title Classification LogitBoost With Simple Regression Functions As Base Learners
+#' @author damirpolat
+#' @name mlr_learners_classif.SimpleLogistic
+#'
+#' @description
+#' LogitBoost with simple regression functions as base learners
+#' Calls [RWeka::make_Weka_classifier('weka/classifiers/functions/SimpleLogistic')] from \CRANpkg{RWeka}.
+#'
+#' @section Custom mlr3 parameters:
+#' - `output_debug_info`:
+#'   - original id: output-debug-info
+#'
+#' - `do_not_check_capabilities`:
+#'   - original id: do-not-check-capabilities
+#'
+#' - `num_decimal_places`:
+#'   - original id: num-decimal-places
+#'
+#' - `batch_size`:
+#'   - original id: batch-size
+#'   
+#' - Reason for change: This learner contains changed ids of the following control arguments
+#' since their ids contain irregular pattern
+#'
+#' @templateVar id classif.SimpleLogistic
+#' @template learner
+#'
+#' @references
+#' `r format_bib(landwehr2005logistic, Sumner2005)`
+#'
+#' @template seealso_learner
+#' @template example
+#' @export
+LearnerClassifSimpleLogistic = R6Class("LearnerClassifSimpleLogistic",
+  inherit = LearnerClassif,
+  public = list(
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    initialize = function() {
+      param_set = ps(
+        subset = p_uty(tags = "train"),
+        na.action = p_uty(tags = "train"),
+        I = p_int(tags = "train"),
+        S = p_lgl(default = FALSE, tags = "train"),
+        P = p_lgl(default = FALSE, tags = "train"),
+        M = p_int(tags = "train"),
+        H = p_int(default = 50L, tags = "train"),
+        W = p_dbl(default = 0L, tags = "train"),
+        A = p_lgl(default = FALSE, tags = "train"),
+        output_debug_info = p_lgl(default = FALSE, tags = "train"),
+        do_not_check_capabilities = p_lgl(default = FALSE,
+                                          tags = "train"),
+        num_decimal_places = p_int(default = 2L, lower = 1L,
+                                   tags = "train"),
+        batch_size = p_int(default = 100L, lower = 1L, tags = "train"),
+        options = p_uty(default = NULL, tags = "train")
+      )
+
+      super$initialize(
+        id = "classif.SimpleLogistic",
+        packages = "RWeka",
+        feature_types = c("logical", "integer", "numeric", "factor", "ordered"),
+        predict_types = c("response", "prob"),
+        param_set = param_set,
+        properties = c("missings", "multiclass", "twoclass"),
+        man = "mlr3extralearners::mlr_learners_classif.SimpleLogistic",
+        label = "Logitboost With Simple Regression Functions As Base Learners"
+      )
+    }
+  ),
+  
+  private = list(
+    .train = function(task) {
+      weka_learner = RWeka::make_Weka_classifier('weka/classifiers/functions/SimpleLogistic')
+      
+      pars = self$param_set$get_values(tags = "train")
+      ctrl_arg_names = weka_control_args(weka_learner)
+      arg_names = setdiff(names(pars), ctrl_arg_names)
+      ctrl = pars[which(names(pars) %in% ctrl_arg_names)]
+      pars = pars[which(names(pars) %nin% ctrl_arg_names)]
+      
+      if (length(ctrl) > 0L) {
+        names(ctrl) = gsub("_", replacement = "-", x = names(ctrl))
+        ctrl = invoke(RWeka::Weka_control, .args = ctrl)
+      }
+      
+      formula = task$formula()
+      data = task$data()
+      invoke(weka_learner, formula = formula, data = data, control = ctrl)
+    },
+    
+    .predict = function(task) {
+      response = NULL
+      prob = NULL
+      pars = self$param_set$get_values(tags = "predict")
+      newdata = ordered_features(task, self)
+      
+      if (self$predict_type == "response") {
+        response = invoke(predict, self$model, newdata = newdata, type = "class",
+                          .args = pars
+        )
+      } else {
+        prob = invoke(predict, self$model, newdata = newdata, type = "prob",
+                      .args = pars
+        )
+      }
+      list(response = response, prob = prob)
+    }
+  )
+)
+
+.extralrns_dict$add("classif.SimpleLogistic", LearnerClassifSimpleLogistic)
---
+++ tests/testthat/test_paramtest_RWeka_classif_SimpleLogistic.R
@@ -0,0 +1,27 @@
+skip_on_os("windows")
+
+test_that("classif.SimpleLogistic train", {
+  learner = lrn("classif.SimpleLogistic")
+  fun = RWeka::make_Weka_classifier('weka/classifiers/functions/SimpleLogistic')
+  exclude = weka_control_args(fun)
+  # formula and data are handled via mlr3
+  # mlr3 does not have the `control` argument because the parameters can be specified directly
+  exclude = c("formula", "data", "control", exclude)
+  paramtest = run_paramtest(learner, fun, exclude, tag = "train")
+  expect_paramtest(paramtest)
+  control_args = weka_control_args(fun)
+  expect_true(all(control_args %in% learner$param_set$ids()))
+})
+
+test_that("classif.SimpleLogistic predict", {
+  # Here we test that the learner implements those arguments that are passed via the
+  # control argument to RWeka::make_Weka_classifier('weka/classifiers/functions/SimpleLogistic')
+  learner = lrn("classif.SimpleLogistic")
+  exclude = c( # all handled by mlr3
+    "object",
+    "newdata",
+    "type"
+  )
+  paramtest = run_paramtest(learner, RWeka:::predict.Weka_classifier, exclude, tag = "predict") # nolint
+  expect_paramtest(paramtest)
+})
