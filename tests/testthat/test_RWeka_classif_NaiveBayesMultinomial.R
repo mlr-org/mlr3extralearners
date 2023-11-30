8df7ff80d93d9ea079f25b65642529994ed73516
damirpolat <damirpolat@protonmail.com>
2023-11-29 23:33:19 -0700

    fix: remove tasks from tests

--- tests/testthat/test_RWeka_classif_NaiveBayesMultinomial.R
+++ tests/testthat/test_RWeka_classif_NaiveBayesMultinomial.R
@@ -3,6 +3,6 @@ skip_on_os("windows")
 test_that("autotest", {
   learner = lrn("classif.NaiveBayesMultinomial")
   expect_learner(learner)
-  result = run_autotest(learner)
+  result = run_autotest(learner, exclude = "missings_binary")
   expect_true(result, info = result$error)
 })
