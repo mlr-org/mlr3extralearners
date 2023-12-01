8ff8499acddf91a18dd8ba5b4b24f1df651beac4
john <bblodfon@gmail.com>
2023-12-01 19:01:18 +0100

    refactor tests using with_seed()

--- tests/testthat/test_partykit_surv_cforest.R
+++ tests/testthat/test_partykit_surv_cforest.R
@@ -6,16 +6,17 @@ test_that("autotest", {
 })
 
 test_that("correct prediction types", {
-  set.seed(42)
-  task = tsk("rats")$filter(sample(1:300, 50))
-  part = partition(task, ratio = 0.9)
-  train_rows = part$train
-  test_rows = part$test
-  unique_times = task$unique_times(train_rows)
+  with_seed(42, {
+    task = tsk("rats")$filter(sample(1:300, 50))
+    part = partition(task, ratio = 0.9)
+    train_rows = part$train
+    test_rows = part$test
+    unique_times = task$unique_times(train_rows)
 
-  learner = lrn("surv.cforest", ntree = 5)
-  p = learner$train(task, part$train)$predict(task, test_rows)
-  expect_matrix(p$data$distr, nrows = length(test_rows),
-                max.cols = length(unique_times))
-  expect_numeric(p$crank, len = length(test_rows))
+    learner = lrn("surv.cforest", ntree = 5)
+    p = learner$train(task, part$train)$predict(task, test_rows)
+    expect_matrix(p$data$distr, nrows = length(test_rows),
+                  max.cols = length(unique_times))
+    expect_numeric(p$crank, len = length(test_rows))
+  })
 })
