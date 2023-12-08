37cb695f1a9f270173a1317c00d58628be67d107
Sebastian Fischer <sebf.fischer@gmail.com>
2023-12-08 18:47:21 +0100

    disable debugging R6 method

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -3,8 +3,10 @@ library(mlr3)
 library(mlr3extralearners)
 library(mlr3proba)
 
-lapply(list.files(system.file("testthat", package = "mlr3"),
-  pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
+tmp = list.files(system.file("testthat", package = "mlr3"),
+  pattern = "^helper.*\\.[rR]", full.names = TRUE)
+tmp = tmp[basename(tmp) != "helper_debugging.R"]
+lapply(tmp, source)
 
 lapply(list.files(system.file("testthat", package = "mlr3proba"),
   pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
