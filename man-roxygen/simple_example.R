ea2dd65136661eb26734603253dadaf2cce2db57
Zhaolin Xu <96219790+Brandonxzl@users.noreply.github.com>
2023-12-20 22:59:39 +0100

    fix line length

--- man-roxygen/simple_example.R
+++ man-roxygen/simple_example.R
@@ -12,7 +12,7 @@ if ("LearnerClassif" %in% class(lrn(id))) {
 %>
 #'
 #' @examples
-#' if (<%= paste0("requireNamespace(\"", pkgs, "\", quietly = TRUE)", collapse = " && ") %>) {
+#' <%= sprintf("if (mlr3misc::require_namespaces(lrn(\"%s\")$packages, quietly = TRUE))", id) %> {
 #' # Define the Learner and set parameter values
 #' <%= sprintf("learner = lrn(\"%s\")", id)%>
 #' print(learner)
