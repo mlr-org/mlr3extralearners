d0e1f33b82b5fe294e31fa03961ceb8aa661bf3f
john <bblodfon@gmail.com>
2024-03-11 12:18:52 +0100

    update parametric learner tests

--- tests/testthat/test_survival_surv_parametric.R
+++ tests/testthat/test_survival_surv_parametric.R
@@ -2,23 +2,23 @@ test_that("autotest aft", {
   set.seed(1)
   learner = lrn("surv.parametric")
   expect_learner(learner)
-  result = run_autotest(learner, check_replicable = FALSE)
+  result = run_autotest(learner, check_replicable = FALSE, exclude = "utf8_feature_names")
   expect_true(result, info = result$error)
 })
 
 test_that("autotest ph", {
   set.seed(1)
-  learner = lrn("surv.parametric", type = "ph")
+  learner = lrn("surv.parametric", form = "ph", discrete = TRUE)
   expect_learner(learner)
-  result = run_autotest(learner, check_replicable = FALSE)
+  result = run_autotest(learner, check_replicable = FALSE, exclude = "utf8_feature_names")
   expect_true(result, info = result$error)
 })
 
 test_that("autotest po", {
   set.seed(1)
-  learner = lrn("surv.parametric", type = "po")
+  learner = lrn("surv.parametric", form = "po")
   expect_learner(learner)
-  result = run_autotest(learner, check_replicable = FALSE)
+  result = run_autotest(learner, check_replicable = FALSE, exclude = "utf8_feature_names")
   expect_true(result, info = result$error)
 })
 
@@ -31,26 +31,25 @@ task = TaskSurv$new("param",
   event = "status")
 
 test_that("manualtest - aft", {
-  learner = lrn("surv.parametric", dist = "weibull", type = "aft")
+  learner = lrn("surv.parametric", dist = "weibull", form = "aft")
   expect_silent(learner$train(task))
   p = learner$predict(task)
   expect_prediction_surv(p)
-  expect_equal(-p$lp, predict(learner$model$fit, type = "lp"))
+  expect_equal(-p$lp, predict(learner$model$model, type = "lp"))
   expect_equal(p$distr[1]$survival(predict(
-    learner$model$fit, type = "quantile", p = c(0.2, 0.8)
+    learner$model$model, type = "quantile", p = c(0.2, 0.8)
   )[1, ]), c(0.8, 0.2))
   expect_equal(p$distr[10]$cdf(predict(
-    learner$model$fit, type = "quantile", p = seq.int(0, 1, 0.1)
+    learner$model$model, type = "quantile", p = seq.int(0, 1, 0.1)
   )[10, ]),
   seq.int(0, 1, 0.1))
 
-  learner = lrn("surv.parametric", dist = "lognormal", type = "aft")$train(task)
+  learner = lrn("surv.parametric", dist = "lognormal", form = "aft")$train(task)
   p = learner$predict(task)
   expect_equal(p$distr[15]$cdf(predict(
-    learner$model$fit, type = "quantile", p = seq.int(0, 1, 0.1)
+    learner$model$model, type = "quantile", p = seq.int(0, 1, 0.1)
   )[15, ]), seq.int(0, 1, 0.1))
 })
-
 
 test_that("missing", {
   learner = lrn("surv.parametric")
@@ -59,71 +58,71 @@ test_that("missing", {
 })
 
 test_that("quantile type", {
-  learner = lrn("surv.parametric", dist = "weibull", type = "aft")$train(task)
-  p = lrn("surv.parametric", dist = "weibull", type = "aft")$train(task)$predict(task)
+  learner = lrn("surv.parametric", dist = "weibull", form = "aft")$train(task)
+  p = lrn("surv.parametric", dist = "weibull", form = "aft")$train(task)$predict(task)
   quantile = p$distr$quantile(c(0.2, 0.8))
   expect_equal(matrix(t(quantile), ncol = 2),
-    predict(learner$model$fit, type = "quantile", p = c(0.2, 0.8)))
+    predict(learner$model$model, type = "quantile", p = c(0.2, 0.8)))
   quantile = p$distr$quantile(0.5)
   expect_equal(unlist(p$distr$cdf(quantile), use.names = FALSE), rep(0.5, 227))
 
-  p = lrn("surv.parametric", dist = "weibull", type = "ph")$train(task)$predict(task)
+  p = lrn("surv.parametric", dist = "weibull", form = "ph")$train(task)$predict(task)
   quantile = p$distr$quantile(0.5)
   expect_equal(unlist(p$distr$cdf(quantile), use.names = FALSE), rep(0.5, 227))
 
-  p = lrn("surv.parametric", dist = "weibull", type = "po")$train(task)$predict(task)
+  p = lrn("surv.parametric", dist = "weibull", form = "po")$train(task)$predict(task)
   quantile = p$distr$quantile(0.5)
   expect_equal(unlist(p$distr$cdf(quantile), use.names = FALSE), rep(0.5, 227))
 })
 
 test_that("quantile dist", {
-  learner = lrn("surv.parametric", dist = "weibull", type = "aft")$train(task)
+  learner = lrn("surv.parametric", dist = "weibull", form = "aft")$train(task)
   p = learner$predict(task)
   quantile = p$distr$quantile(c(0.2, 0.8))
   expect_equal(matrix(t(quantile), ncol = 2),
-    predict(learner$model$fit, type = "quantile", p = c(0.2, 0.8)))
+    predict(learner$model$model, type = "quantile", p = c(0.2, 0.8)))
 
-  learner = lrn("surv.parametric", dist = "exponential", type = "aft")$train(task)
+  learner = lrn("surv.parametric", dist = "exponential", form = "aft")$train(task)
   p = learner$predict(task)
   quantile = p$distr$quantile(c(0.2, 0.8))
   expect_equal(matrix(t(quantile), ncol = 2),
-    predict(learner$model$fit, type = "quantile", p = c(0.2, 0.8)))
+    predict(learner$model$model, type = "quantile", p = c(0.2, 0.8)))
 
-  learner = lrn("surv.parametric", dist = "gaussian", type = "tobit")$train(task)
+  learner = lrn("surv.parametric", dist = "gaussian", form = "tobit")$train(task)
   p = learner$predict(task)
   quantile = p$distr$quantile(c(0.2, 0.8))
   expect_equal(matrix(t(quantile), ncol = 2),
-    predict(learner$model$fit, type = "quantile", p = c(0.2, 0.8)))
+    predict(learner$model$model, type = "quantile", p = c(0.2, 0.8)))
 
   learner = lrn("surv.parametric", dist = "lognormal")$train(task)
   p = learner$predict(task)
   quantile = p$distr$quantile(c(0.2, 0.8))
   expect_equal(matrix(t(quantile), ncol = 2),
-    predict(learner$model$fit, type = "quantile", p = c(0.2, 0.8)))
+    predict(learner$model$model, type = "quantile", p = c(0.2, 0.8)))
 })
 
 test_that("cdf dist", {
-  learner = lrn("surv.parametric", dist = "weibull", type = "aft")$train(task)
+  learner = lrn("surv.parametric", dist = "weibull", form = "aft")$train(task)
   p = learner$predict(task, row_ids = 151:200)
-  cdf = predict(learner$model$fit, type = "quantile", p = c(0.2, 0.8))[151:200, ]
+  cdf = predict(learner$model$model, type = "quantile", p = c(0.2, 0.8))[151:200, ]
   expect_equal(unname(as.matrix(p$distr$cdf(data = t(cdf)))),
     matrix(c(rep(0.2, 50), rep(0.8, 50)), byrow = TRUE, nrow = 2))
 
-  learner = lrn("surv.parametric", dist = "exponential", type = "aft")$train(task)
+  learner = lrn("surv.parametric", dist = "exponential", form = "aft")$train(task)
   p = learner$predict(task, row_ids = 151:200)
-  cdf = predict(learner$model$fit, type = "quantile", p = c(0.2, 0.8))[151:200, ]
+  cdf = predict(learner$model$model, type = "quantile", p = c(0.2, 0.8))[151:200, ]
   expect_equal(unname(as.matrix(p$distr$cdf(data = t(cdf)))),
     matrix(c(rep(0.2, 50), rep(0.8, 50)), byrow = TRUE, nrow = 2))
 
-  learner = lrn("surv.parametric", dist = "gaussian", type = "tobit")$train(task)
+  learner = lrn("surv.parametric", dist = "gaussian", form = "tobit")$train(task)
   p = learner$predict(task, row_ids = 151:200)
-  cdf = predict(learner$model$fit, type = "quantile", p = c(0.2, 0.8))[151:200, ]
+  cdf = predict(learner$model$model, type = "quantile", p = c(0.2, 0.8))[151:200, ]
   expect_equal(unname(as.matrix(p$distr$cdf(data = t(cdf)))),
     matrix(c(rep(0.2, 50), rep(0.8, 50)), byrow = TRUE, nrow = 2))
 
   learner = lrn("surv.parametric", dist = "lognormal")$train(task)
   p = learner$predict(task, row_ids = 151:200)
-  cdf = predict(learner$model$fit, type = "quantile", p = c(0.2, 0.8))[151:200, ]
+  cdf = predict(learner$model$model, type = "quantile", p = c(0.2, 0.8))[151:200, ]
   expect_equal(unname(as.matrix(p$distr$cdf(data = t(cdf)))),
     matrix(c(rep(0.2, 50), rep(0.8, 50)), byrow = TRUE, nrow = 2))
 })
@@ -135,12 +134,12 @@ test_that("loglogistic", {
   p = learner$predict(task)
   quantile = p$distr$quantile(c(0.2, 0.8))
   expect_equal(matrix(t(quantile), ncol = 2),
-    predict(learner$model$fit, type = "quantile", p = c(0.2, 0.8)))
+    predict(learner$model$model, type = "quantile", p = c(0.2, 0.8)))
 
 
   learner = lrn("surv.parametric", dist = "loglogistic")$train(task)
   p = learner$predict(task, row_ids = 151:200)
-  cdf = predict(learner$model$fit, type = "quantile", p = c(0.2, 0.8))[151:200, ]
+  cdf = predict(learner$model$model, type = "quantile", p = c(0.2, 0.8))[151:200, ]
   expect_equal(unname(as.matrix(p$distr$cdf(data = t(cdf)))),
     matrix(c(rep(0.2, 50), rep(0.8, 50)), byrow = TRUE, nrow = 2))
 })
@@ -160,8 +159,8 @@ test_that("discrete - aft", {
 })
 
 test_that("discrete - tobit", {
-  p_cont = lrn("surv.parametric", discrete = FALSE, type = "tobit")$train(task)$predict(task)
-  p_disc = lrn("surv.parametric", discrete = TRUE, type = "tobit")$train(task)$predict(task)
+  p_cont = lrn("surv.parametric", discrete = FALSE, form = "tobit")$train(task)$predict(task)
+  p_disc = lrn("surv.parametric", discrete = TRUE, form = "tobit")$train(task)$predict(task)
   expect_equal(p_cont$lp, p_disc$lp)
   expect_true(inherits(p_disc$distr, "Matdist"))
   s_cont = as.matrix(p_cont$distr$survival(task$unique_times()))
@@ -171,8 +170,8 @@ test_that("discrete - tobit", {
 })
 
 test_that("discrete - ph", {
-  p_cont = lrn("surv.parametric", discrete = FALSE, type = "ph")$train(task)$predict(task)
-  p_disc = lrn("surv.parametric", discrete = TRUE, type = "ph")$train(task)$predict(task)
+  p_cont = lrn("surv.parametric", discrete = FALSE, form = "ph")$train(task)$predict(task)
+  p_disc = lrn("surv.parametric", discrete = TRUE, form = "ph")$train(task)$predict(task)
   expect_equal(p_cont$lp, p_disc$lp)
   expect_true(inherits(p_disc$distr, "Matdist"))
   s_cont = as.matrix(p_cont$distr$survival(task$unique_times()))
@@ -182,8 +181,8 @@ test_that("discrete - ph", {
 })
 
 test_that("discrete - po", {
-  p_cont = lrn("surv.parametric", discrete = FALSE, type = "po")$train(task)$predict(task)
-  p_disc = lrn("surv.parametric", discrete = TRUE, type = "po")$train(task)$predict(task)
+  p_cont = lrn("surv.parametric", discrete = FALSE, form = "po")$train(task)$predict(task)
+  p_disc = lrn("surv.parametric", discrete = TRUE, form = "po")$train(task)$predict(task)
   expect_equal(p_cont$lp, p_disc$lp)
   expect_true(inherits(p_disc$distr, "Matdist"))
   s_cont = as.matrix(p_cont$distr$survival(task$unique_times()))
