95c26a67bca20d13c4903884e1e769ac3f9631ba
john <bblodfon@gmail.com>
2024-08-15 16:26:34 +0200

    fix more notes

--- tests/testthat/test_mboost_surv_glmboost.R
+++ tests/testthat/test_mboost_surv_glmboost.R
@@ -1,16 +1,18 @@
 test_that("autotest ph", {
   learner = lrn("surv.glmboost", family = "coxph")
   expect_learner(learner)
+  exclude_str = "weights|utf8_feature_names|feat_single_factor|feat_all"
   # weights are fine for all predict types except 'distr'
-  result = run_autotest(learner, exclude = "weights|utf8_feature_names|feat_single_factor|feat_all", check_replicable = FALSE)
+  result = run_autotest(learner, exclude = exclude_str, check_replicable = FALSE)
   expect_true(result, info = result$error)
 })
 
 test_that("autotest aft", {
   learner = lrn("surv.glmboost", family = "weibull")
   expect_learner(learner)
+  exclude_str = "weights|utf8_feature_names|feat_single_factor|feat_all"
   # weights are fine for all predict types except 'distr'
-  result = run_autotest(learner, exclude = "weights|utf8_feature_names|feat_single_factor|feat_all", check_replicable = FALSE)
+  result = run_autotest(learner, exclude = exclude_str, check_replicable = FALSE)
   expect_true(result, info = result$error)
 })
 
