0533abe4555f69f2b0998b34ab73e5dba78636f2
be-marc <marcbecker@posteo.de>
2024-08-18 10:36:39 +0200

    chore: change xgboost default nrounds from 1 to 1000

--- R/learner_xgboost_surv_xgboost.R
+++ R/learner_xgboost_surv_xgboost.R
@@ -15,7 +15,7 @@
 #' @template note_xgboost
 #'
 #' @section Initial parameter values:
-#' - `nrounds` is initialized to 1.
+#' - `nrounds` is initialized to 1000.
 #' - `nthread` is initialized to 1 to avoid conflicts with parallelization via \CRANpkg{future}.
 #' - `verbose` is initialized to 0.
 #' - `objective` is initialized to `survival:cox` for survival analysis.
@@ -116,7 +116,7 @@ LearnerSurvXgboost = R6Class("LearnerSurvXgboost",
       ps$add_dep("aft_loss_distribution_scale", "objective", CondEqual$new("survival:aft"))
 
       # custom defaults
-      ps$values = list(nrounds = 1L, nthread = 1L, verbose = 0L, early_stopping_set = "none")
+      ps$values = list(nrounds = 1000L, nthread = 1L, verbose = 0L, early_stopping_set = "none")
 
       super$initialize(
         id = "surv.xgboost",
