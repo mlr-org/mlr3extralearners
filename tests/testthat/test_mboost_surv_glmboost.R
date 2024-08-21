d7b16f07bc04ab955e85f85831ec7aebdc938731
vlegoff <160844540+vlegoff@users.noreply.github.com>
2024-08-21 16:46:56 +0200

    Fixing stackoverflow error on surv.glmboost (#354)
    * Avoid formula interface surv_glmboost learner
    
    * fixed variable with incorrect name
    
    * updocs
    
    * glmboost update: add matrix train interface, enable importance() and selected_features()
    
    * updocs
    
    * update test
    
    * add test for task with factor features
    
    * fix styling notes
    
    * fix more notes
    
    * revert default center to TRUE
    
    ---------
    
    Co-authored-by: Sebastian Fischer <sebf.fischer@gmail.com>
    Co-authored-by: john <bblodfon@gmail.com>
    Co-authored-by: John Zobolas <bblodfon@users.noreply.github.com>

--- tests/testthat/test_mboost_surv_glmboost.R
+++ tests/testthat/test_mboost_surv_glmboost.R
@@ -1,15 +1,26 @@
 test_that("autotest ph", {
   learner = lrn("surv.glmboost", family = "coxph")
   expect_learner(learner)
+  exclude_str = "weights|utf8_feature_names|feat_single_factor|feat_all"
   # weights are fine for all predict types except 'distr'
-  result = run_autotest(learner, exclude = "weights|utf8_feature_names", check_replicable = FALSE)
+  result = run_autotest(learner, exclude = exclude_str, check_replicable = FALSE)
   expect_true(result, info = result$error)
 })
 
 test_that("autotest aft", {
   learner = lrn("surv.glmboost", family = "weibull")
   expect_learner(learner)
+  exclude_str = "weights|utf8_feature_names|feat_single_factor|feat_all"
   # weights are fine for all predict types except 'distr'
-  result = run_autotest(learner, exclude = "weights|utf8_feature_names", check_replicable = FALSE)
+  result = run_autotest(learner, exclude = exclude_str, check_replicable = FALSE)
   expect_true(result, info = result$error)
+})
+
+test_that("can't get importance or selected features on task with factors", {
+  task = tsk("lung")
+  learner = lrn("surv.glmboost")
+  learner$train(task)
+  expect_class(learner$model, "glmboost")
+  expect_error(learner$importance())
+  expect_error(learner$selected_features())
 })
