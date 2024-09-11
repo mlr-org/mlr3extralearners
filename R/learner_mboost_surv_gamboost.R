abdd8cf334e0f9e60810e3ebc879228a833ed549
john <bblodfon@gmail.com>
2024-09-11 09:58:53 +0200

    update gamboost doc

--- R/learner_mboost_surv_gamboost.R
+++ R/learner_mboost_surv_gamboost.R
@@ -9,8 +9,20 @@
 #' @template learner
 #' @templateVar id surv.gamboost
 #'
-#' @details
-#' `distr` prediction made by [mboost::survFit()].
+#' @section Prediction types:
+#' This learner returns two to three prediction types:
+#' 1. `lp`: a vector containing the linear predictors (relative risk scores),
+#' where each score corresponds to a specific test observation.
+#' Calculated using [mboost::predict.gamboost()].
+#' If the `family` parameter is not `"coxph"`, `-lp` is returned, since non-coxph
+#' families represent AFT-style distributions where lower `lp` values indicate higher risk.
+#' 2. `crank`: same as `lp`.
+#' 3. `distr`: a survival matrix in two dimensions, where observations are
+#' represented in rows and time points in columns.
+#' Calculated using [mboost::survFit()].
+#' This prediction type is present only when the `family` distribution parameter
+#' is equal to `"coxph"`.
+#' By default the Breslow estimator is used for computing the baseline hazard.
 #'
 #' @references
 #' `r format_bib("buhlmann2003boosting")`
@@ -57,7 +69,7 @@ LearnerSurvGAMBoost = R6Class("LearnerSurvGAMBoost",
         id = "surv.gamboost",
         param_set = ps,
         feature_types = c("integer", "numeric", "factor", "logical"),
-        predict_types = c("distr", "crank", "lp"),
+        predict_types = c("crank", "lp", "distr"),
         properties = c("weights", "importance", "selected_features"),
         packages = c("mlr3extralearners", "mboost", "pracma"),
         man = "mlr3extralearners::mlr_learners_surv.gamboost",
@@ -138,7 +150,6 @@ LearnerSurvGAMBoost = R6Class("LearnerSurvGAMBoost",
       pars = pars[!(names(pars) %in% formalArgs(mboost::Cindex))]
       pars = pars[!(names(pars) %in% c("family", "custom.family"))]
 
-
       with_package("mboost", {
         invoke(mboost::gamboost,
           formula = task$formula(task$feature_names),
@@ -158,21 +169,10 @@ LearnerSurvGAMBoost = R6Class("LearnerSurvGAMBoost",
       if (is.null(self$param_set$values$family) || self$param_set$values$family == "coxph") {
         survfit = invoke(mboost::survFit, self$model, newdata = newdata)
 
-        mlr3proba::.surv_return(times = survfit$time,
-          surv = t(survfit$surv),
-          lp = lp)
+        mlr3proba::.surv_return(times = survfit$time, surv = t(survfit$surv), lp = lp)
       } else {
         mlr3proba::.surv_return(lp = -lp)
       }
-
-
-      # FIXME - RE-ADD ONCE INTERPRETATION IS CLEAR
-      # response = NULL
-      # if (!is.null(self$param_set$values$family)) {
-      #   if (self$param_set$values$family %in% c("weibull", "loglog", "lognormal", "gehan")) {
-      #     response = exp(lp)
-      #   }
-      # }
     }
   )
 )
