c39cf16f869e0f92f921ce29e53cc20fa84f19c6
AnnaNzrv <anna.nazarova@outlook.com>
2025-03-13 17:54:35 +0100

    quick fix in paramtest for regr and classif

--- tests/testthat/test_paramtest_aorsf_regr_aorsf.R
+++ tests/testthat/test_paramtest_aorsf_regr_aorsf.R
@@ -1,6 +1,6 @@
 test_that("paramtest regr.aorsf train", {
   learner = lrn("regr.aorsf")
-  fun = aorsf::aorsf
+  fun_list = list(aorsf::orsf)
   exclude = c(
     "control", # handled internally
     "weights", # handled internally
@@ -9,6 +9,7 @@ test_that("paramtest regr.aorsf train", {
     "formula", # handled internally
     "control", # handled internally
     "oobag_fun", # handled internally
+    "oobag_pred_horizon", # only relevant for survival trees
     "tree_seeds", # handled internally
     "new_data", # handled internally
     "object", # handled internally
@@ -25,23 +26,24 @@ test_that("paramtest regr.aorsf train", {
 
   # note that you can also pass a list of functions in case $.train calls more than one
   # function, e.g. for control arguments
-  paramtest = run_paramtest(learner, fun, exclude, tag = "train")
+  paramtest = run_paramtest(learner, fun_list, exclude, tag = "train")
   expect_paramtest(paramtest)
 })
 
 test_that("paramtest regr.aorsf predict", {
   learner = lrn("regr.aorsf")
-  fun = aorsf::predict # nolint
+  fun_list = list(aorsf:::predict.ObliqueForest)
   exclude = c(
     "object", # handled internally
     "new_data", # handled internally
     "pred_horizon", # all times required for distr
+    "pred_simplify", #can't be FALSE otherwise response is NA in prediction
     "pred_type", # handled internally
-    "na_action", # not implemented
+    "na_action", # not implemented,
     "verbose_progress", # handled internally
     "boundary_checks" # not implemented
   )
 
-  paramtest = run_paramtest(learner, fun, exclude, tag = "predict")
+  paramtest = run_paramtest(learner, fun_list, exclude, tag = "predict")
   expect_paramtest(paramtest)
 })
