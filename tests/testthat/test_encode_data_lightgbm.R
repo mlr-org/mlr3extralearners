5395a7e3a140bd593d7b8733b24bc23411529a2d
AnnaNzrv <anna.nazarova@outlook.com>
2025-03-21 11:57:08 +0100

    fix ci test as in pr 414

--- tests/testthat/test_encode_data_lightgbm.R
+++ tests/testthat/test_encode_data_lightgbm.R
@@ -11,7 +11,7 @@ test_that("encode_data_lightgbm works for train", {
 
   newdata = data.table::data.table(y = 1, x_fct = factor("zzz"), x_lgl = TRUE)
 
-  expect_error(learner$predict_newdata(newdata), "There is a mismatch")
+  expect_error(learner$predict_newdata(newdata), "received task with different column info")
 
   expect_error(learner$predict(task), regexp = NA)
   expect_error(learner$train(task), regexp = NA)
