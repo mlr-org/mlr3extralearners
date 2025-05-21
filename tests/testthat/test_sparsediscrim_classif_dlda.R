b4d17c0dfaa9b6c9065173afb8d9f6bae64993b4
AnnaNzrv <anna.nazarova@outlook.com>
2025-05-21 18:59:45 +0200

    add diagonal lda learner from sparsediscrim

---
+++ tests/testthat/test_sparsediscrim_classif_dlda.R
@@ -0,0 +1,7 @@
+test_that("autotest", {
+  learner = lrn("classif.dlda")
+  expect_learner(learner, check_man = FALSE)
+  # note that you can skip tests using the exclude argument
+  result = run_autotest(learner)
+  expect_true(result, info = result$error)
+})
