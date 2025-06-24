efccbd5923e44ddc2b02b438250eae596f22f994
Nikolai German <129080480+NikoGerman@users.noreply.github.com>
2025-06-24 09:42:28 +0200

    added description https://github.com/mlr-org/mlr3extralearners/issues/365#

--- R/learner_ExhaustiveSearch_classif_glm.R
+++ R/learner_ExhaustiveSearch_classif_glm.R
@@ -1,32 +1,78 @@
+#' @title Logistic Regression with Exhaustive Search
+#' @author ngerman
+#' @name mlr_learners_classif.exhaustive_search
+#'
+#' @description
+#' Logistic Regression, where the features used in the model are selected by running Exhaustive Search.
+#'
+#' Calls [ExhaustiveSearch::ExhaustiveSearch()] from \CRANpkg{ExhaustiveSearch}.
+#'
+#' @section Initial parameter values:
+#' - `family`:
+#'   - Actual default: NULL
+#'   - Adjusted default: "binomial"
+#'   - Reason for change: To comply with mlr3 architecture, we differentiate between classification and regression learners.
+#' - `nThreads`:
+#'   - Actual default: NULL
+#'   - Adjusted default: 1
+#'   - Reason for change: Suppressing the automatic internal parallelization if
+#'     `cv.folds` > 0.
+#'
+#'
+#' @template learner
+#' @templateVar id classif.exhaustive_search
+#'
+#' @details
+#' Internal validation not yet implemented.
+#'
+#' @export
+#' @template seealso_learner
+#' @examples
+#'
+#' # define the learner
+#' learner = LearnerClassifExhaustiveSearch$new()
+#' learner$predict_type = "prob"
+#' learner$param_set$set_values(combsUpTo = 3)
+#'
+#' # define the task
+#' tsk_gc = tsk("german_credit")
+#'
+#' # train the learner
+#' learner$train(tsk_gc)
+#'
+#' # extract selected features
+#' learner$selected_features()
+#'
+#' # predict on training task
+#' learner$predict(tsk_gc)
+
 library(mlr3)
 library(paradox)
 library(R6)
-library(mlr3misc)
 
-LearnerClassifExhaustiveSearch = R6Class("LearnerRegrExhaustiveSearch",
+LearnerClassifExhaustiveSearch = R6Class("LearnerClassifExhaustiveSearch",
                                       inherit = LearnerClassif,
                                       public = list(
                                         initialize = function() {
                                           param_set = ps(
-                                            family = p_fct(c("gaussian", "binomial"), tags = "train"), # maybe delete option gaussian in classif case?
+                                            family = p_fct(c("gaussian", "binomial"), init = "binomial", tags = "train"),
                                             performanceMeasure = p_fct(c("MSE", "AIC"), tags = "train"), # interdepency to testSetIDs
                                             combsUpTo = p_int(1L, tags = "train"),
                                             nResults = p_int(1L, default = 5000L, tags = "train"),
-                                            nThreads = p_int(1L, tags = "train"), # side effects to parallel?
+                                            nThreads = p_int(1L, init = 1L, tags = "train"),
                                             testSetIDs = p_int(1L, tags = "train"), # use as internal validation?
-                                            errorVal = p_uty(default = -1, tags = "train"),
+                                            errorVal = p_uty(default = -1L, tags = "train"),
                                             quietly = p_lgl(default = FALSE, tags = "train"),
                                             checkLarge = p_lgl(default = TRUE, tags = "train")
                                           )
-                                          param_set$set_values(family = "binomial")
 
                                           super$initialize(
                                             id = "classif.exhaustive_search",
-                                            feature_types = c("logical", "integer", "numeric", "factor", "ordered"),
+                                            feature_types = c("logical", "integer", "numeric", "character", "factor", "ordered"),
                                             predict_types = c("response", "prob"),
                                             packages = "ExhaustiveSearch",
                                             param_set = param_set,
-                                            properties = c("twoclass", "missings", "selected_features"),
+                                            properties = c("twoclass", "selected_features"), # to add: "validation"
                                             label = "Exhaustive Search",
                                             man = "mlr3extralearners::mlr_learners_classif.exhaustive_search"
                                           )
@@ -50,9 +96,9 @@ LearnerClassifExhaustiveSearch = R6Class("LearnerRegrExhaustiveSearch",
                                             .args = pv
                                           )
                                           # extract selected features of best performing model
-                                          # make new task
                                           private$.selected_features = intersect(ES_response$featureNames,
-                                                                                 ExhaustiveSearch::getFeatures(ES_response, ranks = 1))
+                                                                                 ExhaustiveSearch::getFeatures(ES_response, ranks = 1L))
+                                          # task_selected: reduce task to selected features
                                           task_selected = task$clone()
                                           task_selected$select(private$.selected_features)
                                           # return best model
@@ -65,16 +111,19 @@ LearnerClassifExhaustiveSearch = R6Class("LearnerRegrExhaustiveSearch",
                                         },
                                         .predict = function(task) {
                                           # pv = self$param_set$get_values(tags = "predict")
-                                          lvls = c(task$negative, task$positive)
                                           # ensure same column order in train and predict
                                           newdata = mlr3extralearners:::ordered_features(task, self)
 
-                                          p = unname(invoke(predict, object = self$model, newdata = newdata, type = "response"))
+                                          p = unname(invoke(predict,
+                                                            object = self$model,
+                                                            newdata = newdata,
+                                                            type = "response")
+                                                     )
 
                                           if (self$predict_type == "response") {
-                                            list(response = ifelse(p < 0.5, lvls[1L], lvls[2L]))
+                                            list(response = ifelse(p < 0.5, task$negative, task$positive))
                                           } else {
-                                            list(prob = pvec2mat(p, lvls))
+                                            list(prob = mlr3extralearners:::pprob_to_matrix((1-p), task)) # from helpers
                                           }
                                         },
                                         .selected_features = NULL
@@ -82,8 +131,10 @@ LearnerClassifExhaustiveSearch = R6Class("LearnerRegrExhaustiveSearch",
 )
 
 learner = LearnerClassifExhaustiveSearch$new()
-tsk_pima = tsk("pima")
-learner$train(tsk_pima)
-learner$model
-learner$predict(tsk_pima)$score()
+learner$predict_type = "prob"
+learner$param_set$set_values(combsUpTo = 3)
+tsk_gc = tsk("german_credit")
+learner$train(tsk_gc)
 learner$selected_features()
+learner$predict(tsk_gc)
+
