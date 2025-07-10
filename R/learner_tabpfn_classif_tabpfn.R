cef55991fab684f8386a7b7534f5c318d80b7072
john <bblodfon@gmail.com>
2025-07-10 17:27:06 +0200

    fix NOTEs

--- R/learner_tabpfn_classif_tabpfn.R
+++ R/learner_tabpfn_classif_tabpfn.R
@@ -84,14 +84,14 @@ LearnerClassifTabPFN = R6Class("LearnerClassifTabPFN",
     #' @description
     #' Marshal the learner's model.
     #' @param ... (any)\cr
-    #'   Additional arguments passed to [`marshal_model()`].
+    #'   Additional arguments passed to [`marshal_model()`][mlr3::marshaling()].
     marshal = function(...) {
       mlr3::learner_marshal(.learner = self, ...)
     },
     #' @description
     #' Unmarshal the learner's model.
     #' @param ... (any)\cr
-    #'   Additional arguments passed to [`unmarshal_model()`].
+    #'   Additional arguments passed to [`unmarshal_model()`][mlr3::marshaling()].
     unmarshal = function(...) {
       mlr3::learner_unmarshal(.learner = self, ...)
     }
