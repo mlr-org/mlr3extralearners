0a558295ce0e0c292e361b8ed151d17cd06828b0
john <bblodfon@gmail.com>
2025-08-15 22:05:33 +0200

    fix more tests

--- tests/testthat/test_randomPlantedForest_classif_rpf.R
+++ tests/testthat/test_randomPlantedForest_classif_rpf.R
@@ -1,6 +1,8 @@
 test_that("autotest", {
-  learner = lrn("classif.rpf")
-  expect_learner(learner)
-  result = run_autotest(learner)
-  expect_true(result, info = result$error)
+  with_seed(42, {
+    learner = lrn("classif.rpf")
+    expect_learner(learner)
+    result = run_autotest(learner)
+    expect_true(result, info = result$error)
+  })
 })
