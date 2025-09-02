04beac01a7a54cbf3d8bac3c65385bb13aed8af7
AnnaNzrv <anna.nazarova@outlook.com>
2025-09-02 14:41:54 +0200

    feat: neuralnet classif learner from old mlr2

---
+++ tests/testthat/test_paramtest_neuralnet_classif_neuralnet.R
@@ -0,0 +1,23 @@
+test_that("paramtest classif.neuralnet train", {
+  learner = lrn("classif.neuralnet")
+  fun = neuralnet::neuralnet
+    exclude = c(
+      "formula", # handled internally
+      "data" # handled internally
+    )
+  paramtest = run_paramtest(learner, fun, exclude, tag = "train")
+  expect_paramtest(paramtest)
+})
+
+test_that("paramtest classif.neuralnet predict", {
+  learner = lrn("classif.neuralnet")
+  fun = neuralnet:::predict.nn # nolint
+  exclude = c(
+    "object", # handled internally
+    "data", # handled internally
+    "newdata", # handled internally
+    "all.units"  # should always be set to FALSE (default in neuralnet:::predict.nn)
+  )
+  paramtest = run_paramtest(learner, fun, exclude, tag = "predict")
+  expect_paramtest(paramtest)
+})
