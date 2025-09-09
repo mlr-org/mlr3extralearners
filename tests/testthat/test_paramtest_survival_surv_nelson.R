aff2c9d62ac3138934148b714a5ae459a4dbf682
john <bblodfon@gmail.com>
2025-09-09 10:00:32 +0200

    fix small codefactor notes

--- tests/testthat/test_paramtest_survival_surv_nelson.R
+++ tests/testthat/test_paramtest_survival_surv_nelson.R
@@ -4,7 +4,7 @@ skip_if_not_installed("survival")
 test_that("surv.nelson", {
   learner = lrn("surv.nelson")
   fun = survival::survfit
-  exclude = c("formula") # coerced internally
+  exclude = "formula" # coerced internally
   paramtest = run_paramtest(learner, fun, exclude, tag = "train")
   expect_paramtest(paramtest)
 })
