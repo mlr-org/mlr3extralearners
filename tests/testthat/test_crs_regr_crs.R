cac58179d46d90c750e1362a9a6c295e18b6dbda
AnnaNzrv <anna.nazarova@outlook.com>
2025-09-18 12:04:23 +0200

    feat: crs regr learner

--- tests/testthat/test_crs_regr_crs.R
+++ tests/testthat/test_crs_regr_crs.R
@@ -2,7 +2,8 @@ skip_if_not_installed("crs")
 
 test_that("autotest", {
   learner = lrn("regr.crs")
-  expect_learner(learner, check_man = FALSE)
+  expect_learner(learner)
   result = run_autotest(learner, exclude = "single_factor || single_ordered")
+  # crs can't handle tasks with only one feature
   expect_true(result)
 })
