4bf749a118412cbaffe439c53db50b883437649d
john <bblodfon@gmail.com>
2025-10-04 02:34:10 +0300

    remove density learner tests

--- tests/testthat/test_ks_dens_kde_ks.R
+++
@@ -1,8 +0,0 @@
-skip_if_not_installed("ks")
-
-test_that("autotest", {
-  learner = lrn("dens.kde_ks")
-  expect_learner(learner)
-  result = run_autotest(learner)
-  expect_true(result, info = result$error)
-})
--- tests/testthat/test_pendensity_dens_pen.R
+++
@@ -1,9 +0,0 @@
-test_that("autotest", {
-  skip_if_not_installed("pendensity")
-  learner = lrn("dens.pen")
-  expect_learner(learner)
-  capture.output({
-    result = run_autotest(learner)
-  })
-  expect_true(result, info = result$error)
-})
