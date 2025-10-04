d637be987792a6bd971ed3a87497bb3ed2f5a16a
john <bblodfon@gmail.com>
2025-10-05 01:05:35 +0300

    remove capture.output() from test

--- tests/testthat/test_BART_surv_bart.R
+++ tests/testthat/test_BART_surv_bart.R
@@ -4,6 +4,6 @@ test_that("autotest", {
   withr::local_seed(1)
   learner = lrn("surv.bart", nskip = 1, ndpost = 1, keepevery = 1)
   expect_learner(learner)
-  capture.output({result = run_autotest(learner)})
+  result = run_autotest(learner)
   expect_true(result, info = result$error)
 })
