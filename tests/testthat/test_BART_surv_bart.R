30a4aedaf7d393c6c95b652e7901cf1006f4827a
john <bblodfon@gmail.com>
2025-10-05 02:20:51 +0300

    disable mirai encapsulation for surv.bart

--- tests/testthat/test_BART_surv_bart.R
+++ tests/testthat/test_BART_surv_bart.R
@@ -1,9 +1,11 @@
 skip_if_not_installed("BART")
 
 test_that("autotest", {
+  mirai::daemons(0, .compute = "mlr3_encapsulation")
   withr::local_seed(1)
   learner = lrn("surv.bart", nskip = 1, ndpost = 1, keepevery = 1)
   expect_learner(learner)
-  result = run_autotest(learner)
+  capture_output({result = run_autotest(learner)})
   expect_true(result, info = result$error)
+  mirai::daemons(1, .compute = "mlr3_encapsulation")
 })
