bb2eeb3507077b229023735b1eee6ab9b8dd8429
john <bblodfon@gmail.com>
2025-10-05 03:50:20 +0300

    add comment for future reference

--- tests/testthat/test_BART_surv_bart.R
+++ tests/testthat/test_BART_surv_bart.R
@@ -4,6 +4,8 @@ test_that("autotest", {
   withr::local_seed(1)
   learner = lrn("surv.bart", nskip = 1, ndpost = 1, keepevery = 1)
   expect_learner(learner)
+  # We exclude 'feat_all' to disable encapsulated tests since mirai's
+  # nanonext dependency runs into a bug => `panic: nng is not fork-reentrant safe`
   capture_output({result = run_autotest(learner, exclude = "feat_all")})
   expect_true(result, info = result$error)
 })
