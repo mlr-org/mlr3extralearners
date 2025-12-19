344307d5b2fb6a7797849929052a437e23bb7800
john <bblodfon@gmail.com>
2025-12-19 17:52:04 +0200

    rename learner, update doc, add `times` predict parameter

--- R/learner_flexsurv_surv_flexsurvspline.R
+++ R/learner_flexsurv_surv_flexsurvspline.R
@@ -1,18 +1,20 @@
 #' @title Survival Flexible Parametric Spline Learner
 #' @author RaphaelS1
-#' @name mlr_learners_surv.flexible
+#' @name mlr_learners_surv.flexsurvspline
 #'
 #' @description
-#' Flexible parametric spline learner.
+#' Flexible survival regression using the Royston/Parmar spline model.
 #' Calls [flexsurv::flexsurvspline()] from \CRANpkg{flexsurv}.
 #'
 #' @template learner
-#' @templateVar id surv.flexible
+#' @templateVar id surv.flexsurvspline
 #'
 #' @section Prediction types:
 #' This learner returns three prediction types:
 #' 1. `lp`: a vector containing the linear predictors (relative risk scores),
 #' where each score corresponds to a specific test observation.
+#' Higher linear predictor gives higher hazard (equivalently, lower survival)
+#' for the spline models.
 #' Calculated using [flexsurv::flexsurvspline()] and the estimated coefficients.
 #' For fitted coefficients, \eqn{\hat{\beta} = (\hat{\beta_0},...,\hat{\beta_P})},
 #' and the test data covariates \eqn{X^T = (X_0,...,X_P)^T}, where \eqn{X_0}{X0}
@@ -24,18 +26,11 @@
 #' 3. `crank`: same as `lp`.
 #' 4. `response`: mean survival time calculated using `predict.flexsurvreg(type = "response", ...)`
 #'
-#' @section Initial parameter values:
-#' - `k`:
-#'   - Actual default: `0`
-#'   - Initial value: `1`
-#'   - Reason for change: The default value of `0` is equivalent to, and a much less efficient
-#'   implementation of, [LearnerSurvParametric].
-#'
 #' @references
 #' `r format_bib("royston2002flexible")`
 #'
 #' @template seealso_learner
-#' @examplesIf learner_is_runnable("surv.flexible")
+#' @examplesIf learner_is_runnable("surv.flexsurvspline")
 #' library(survival)
 #'
 #' # Define the task, split to train/test set
@@ -44,7 +39,7 @@
 #' part = partition(task)
 #'
 #' # Define the learner
-#' learner = lrn("surv.flexible", k = 1,
+#' learner = lrn("surv.flexsurvspline", k = 1,
 #'   formula = Surv(time, status) ~ age + ph.karno + sex,
 #'   anc = list(gamma1 = ~ sex))
 #'
@@ -59,23 +54,23 @@
 #' # Score the predictions
 #' predictions$score()
 #' @export
-LearnerSurvFlexible = R6Class("LearnerSurvFlexible",
+LearnerSurvFlexSpline = R6Class("LearnerSurvFlexSpline",
   inherit = mlr3proba::LearnerSurv,
 
   public = list(
     #' @description
     #' Creates a new instance of this [R6][R6::R6Class] class.
     initialize = function() {
-      ps = ps(
-        formula = p_uty(tags = c("train", "predict"), custom_check = check_formula),
+      param_set = ps(
+        formula = p_uty(tags = c("train", "predict"), custom_check = checkmate::check_formula),
         bhazard = p_uty(tags = "train"),
+        rtrunc = p_uty(tags = "train"),
         k = p_int(default = 0L, lower = 0L, tags = "train"),
         knots = p_uty(tags = "train"),
         bknots = p_uty(tags = "train"),
         scale = p_fct(default = "hazard", levels = c("hazard", "odds", "normal"), tags = "train"),
         timescale = p_fct(default = "log", levels = c("log", "identity"), tags = "train"),
         spline = p_fct(default = "rp", levels = c("rp", "splines2ns"), tags = "train"),
-        rtrunc = p_uty(tags = "train"),
         # params from `flexsurvreg()`
         inits = p_uty(tags = "train"),
         fixedpars = p_uty(tags = "train"),
@@ -86,21 +81,18 @@ LearnerSurvFlexible = R6Class("LearnerSurvFlexible",
         rel.tolerance = p_dbl(default = 1e-09, tags = "train"),
         toler.chol = p_dbl(default = 1e-10, tags = "train"),
         debug = p_int(default = 0, lower = 0, upper = 1, tags = "train"),
-        outer.max = p_int(default = 10L, tags = "train")
+        outer.max = p_int(default = 10L, tags = "train"),
+        times = p_uty(tags = "predict")
       )
 
-      # value of k is changed as the default is equivalent (and a much more inefficient)
-      # implementation of `surv.parametric`
-      ps$values = list(k = 1)
-
       super$initialize(
-        id = "surv.flexible",
+        id = "surv.flexsurvspline",
         packages = c("mlr3extralearners", "flexsurv", "survival"),
         feature_types = c("logical", "integer", "factor", "numeric"),
-        predict_types = c("crank", "lp", "distr"),
-        param_set = ps,
+        predict_types = c("crank", "lp", "distr", "response"),
+        param_set = param_set,
         properties = "weights",
-        man = "mlr3extralearners::mlr_learners_surv.flexible",
+        man = "mlr3extralearners::mlr_learners_surv.flexsurvspline",
         label = "Flexible Parametric Splines"
       )
     }
@@ -108,24 +100,26 @@ LearnerSurvFlexible = R6Class("LearnerSurvFlexible",
 
   private = list(
     .train = function(task) {
-      pars_train = self$param_set$get_values(tags = "train")
+      pv = self$param_set$get_values(tags = "train")
       args_ctrl = formalArgs(survival::survreg.control)
-      pars_ctrl = pars_train[names(pars_train) %in% args_ctrl]
-      pars_train = pars_train[names(pars_train) %nin% args_ctrl]
-      pars_train$sr.control = invoke(survival::survreg.control, .args = pars_ctrl)
-      pars_train$weights = private$.get_weights(task)
+      pars_ctrl = pv[names(pv) %in% args_ctrl]
+      pv = pv[names(pv) %nin% args_ctrl]
+      pv$sr.control = invoke(survival::survreg.control, .args = pars_ctrl)
+      pv$weights = private$.get_weights(task)
 
-      if (is.null(pars_train$formula)) {
+      if (is.null(pv$formula)) {
         form = task$formula(task$feature_names)
       } else {
-        form = pars_train$formula
-        pars_train$formula = NULL
+        form = pv$formula
+        pv$formula = NULL
       }
 
-      invoke(flexsurv::flexsurvspline,
-             formula = form,
-             data = task$data(),
-             .args = pars_train)
+      invoke(
+        flexsurv::flexsurvspline,
+        formula = form,
+        data = task$data(),
+        .args = pv
+      )
     },
 
     .predict = function(task) {
@@ -191,4 +185,4 @@ predict_flexsurvreg = function(object, task, learner, form) {
   list(crank = lp, lp = lp, distr = surv, response = response)
 }
 
-.extralrns_dict$add("surv.flexible", LearnerSurvFlexible)
+.extralrns_dict$add("surv.flexsurvspline", LearnerSurvFlexSpline)
