7b37f447f34fa5c350bac3fa20728c1c2b752f47
john <bblodfon@gmail.com>
2025-12-19 18:51:18 +0200

    update surv.flexspline learner tests

--- tests/testthat/test_flexsurv_surv_flexible.R
+++ tests/testthat/test_flexsurv_surv_flexible.R
@@ -2,17 +2,17 @@ skip_if_not_installed("flexsurv")
 
 test_that("autotest", {
   withr::local_seed(200)
-  learner = lrn("surv.flexible", k = 1, scale = "normal", inits = c(0.1, 1))
+  learner = lrn("surv.flexspline", k = 1, scale = "normal", inits = c(0.1, 1))
   expect_learner(learner)
   # sanity excluded due to issues in optimisation (it's too easy...)
   result = run_autotest(learner, check_replicable = FALSE, exclude = "sanity")
   expect_true(result, info = result$error)
 })
 
-test_that("manualtest", {
+test_that("manual test", {
   withr::local_seed(1)
   task = tsk("rats")
-  learn = lrn("surv.flexible", k = 1, scale = "normal", inits = c(1, 1, 1))
+  learn = lrn("surv.flexspline", k = 1, scale = "normal", inits = c(1, 1, 1))
   learn$train(task)
   p = learn$predict(task)
   # Comparison to flexsurv
@@ -32,18 +32,21 @@ test_that("manualtest", {
 })
 
 test_that("missing", {
-  learner = lrn("surv.flexible", k = 1)
-  learner$train(tsk("lung"))
-  expect_error(learner$predict(lung_missings))
+  learner = lrn("surv.flexspline", k = 1)
+  task = as_task_surv(x = data.frame(
+    x1 = c(runif(9), NA),
+    event = rbinom(10, 1, 0.5),
+    time = rexp(10)), id = "missing")
+  expect_error(learner$train(task), class = "Mlr3ErrorInput")
 })
 
 test_that("formula interface works", {
-  expect_error(lrn("surv.flexible", formula = "not_a_formula"))
-  expect_error(lrn("surv.flexible", formula = list("not_a_formula")))
+  expect_error(lrn("surv.flexspline", formula = "not_a_formula"))
+  expect_error(lrn("surv.flexspline", formula = list("not_a_formula")))
   task = tsk("lung")
 
   form = survival::Surv(time, status) ~ age + sex
-  learner = lrn("surv.flexible", k = 1, formula = form)
+  learner = lrn("surv.flexspline", k = 1, formula = form)
   learner$train(task)
   expect_equal(learner$model$ncovs, 2)
   expect_equal(learner$model$all.formulae$gamma0, form)
@@ -55,11 +58,11 @@ test_that("formula interface works", {
   expect_prediction_surv(p)
   # check manual model predictions are the same
   model = flexsurv::flexsurvspline(formula = form, data = task$data(), k = 1)
-  lp = predict(model, newdata = task$data(), type = "lp")[[2]]
+  lp = predict(model, newdata = task$data(), type = "lp")[[2L]]
   expect_equal(p$lp, lp)
 
   # use of `anc` changes coefficients
-  learner2 = lrn("surv.flexible", k = 1, formula = form, anc = list(gamma1 = ~ sex))
+  learner2 = lrn("surv.flexspline", k = 1, formula = form, anc = list(gamma1 = ~ sex))
   learner2$train(task)
   expect_equal(learner2$model$ncovs, 2)
   expect_equal(learner2$model$all.formulae$gamma0, form)
@@ -82,6 +85,6 @@ test_that("formula interface works", {
   # check manual model predictions are the same
   model2 = flexsurv::flexsurvspline(formula = form, data = task$data(),
                                     anc = list(gamma1 = ~ sex), k = 1)
-  lp2 = predict(model2, newdata = task$data(), type = "lp")[[2]]
+  lp2 = predict(model2, newdata = task$data(), type = "lp")[[2L]]
   expect_equal(p2$lp, lp2)
 })
