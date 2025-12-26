103828514f83175e5281d97bfc804673f72f8505
john <bblodfon@gmail.com>
2025-12-27 00:39:19 +0200

    fix parameter tests

--- tests/testthat/test_paramtest_xgboost_surv_xgboost.R
+++ tests/testthat/test_paramtest_xgboost_surv_xgboost.R
@@ -1,51 +1,31 @@
-skip_if_not_installed("rvest")
 skip_if_not_installed("mlr3proba")
 skip_if_not_installed("xgboost")
-
-x = rvest::read_html("https://xgboost.readthedocs.io/en/latest/parameter.html")
-xli = rvest::html_elements(x, "li")
-xp = rvest::html_elements(x, "p")
-x = c(rvest::html_text2(xli), rvest::html_text2(xp))
-x = grep("default=", x, value = TRUE)
-x = strsplit(x, split = " ")
-x = mlr3misc::map_chr(x, function(x) x[1])
-x = gsub(",", replacement = "", x)
-# these are defined on the same line as colsample_bytree and cannot be scraped therefore
-x = append(x, values = c("colsample_bylevel", "colsample_bynode", "base_score"))
-# values which do not match regex
-x = append(x, values = c("interaction_constraints", "monotone_constraints", "base_score"))
-# only defined in help page but not in signature or website
-x = append(x, values = "lambda_bias")
-add_params_xgboost = x
 
 test_that("surv.xgboost", {
   learner_cox = lrn("surv.xgboost.cox")
   learner_aft = lrn("surv.xgboost.aft")
-  fun = list(xgboost::xgb.train, xgboost::xgboost, add_params_xgboost)
+  fun = list(xgboost::xgb.train, xgboost::xgb.params)
   exclude = c(
-    "x", # handled by mlr3
     "params", # handled by mlr3
+    "custom_metric", # not supported for survival
     "data", # handled by mlr3
-    "obj", # handled via type parameter
-    "verbosity", # handled by mlr3
-    "seed", # not available in R package
-    "train", # handled by mlr3
-    "task", # handled by mlr3
-    "model_in", # handled by mlr3
-    "model_out", # handled by mlr3
-    "model_dir", # handled by mlr3
-    "dump_format", # CLI parameter, not for R package
-    "name_dump", # CLI parameter, not for R package
-    "name_pred", # CLI parameter, not for R package
-    "pred_margin", # CLI parameter, not for R package
-    "eval_metric", # handled by mlr3
     "objective", # handled by mlr3
-    "label", # handled by mlr3
-    "weight", # handled by mlr3
-    "nthread", # handled by mlr3
-    "early_stopping_set", # handled by mlr3
-    "aft_loss_distribution", # survival specific
-    "aft_loss_distribution_scale" # survival specific
+    "seed", # not available in xgb.params doc?
+    "num_class", # parameters is for multiclassification objectives, not survival
+    "eval_metric", # handled by mlr3
+    "eta", # alias for parameter learning_rate
+    "min_split_loss", # alias for parameter gamma
+    "reg_lambda", # alias for parameter lambda
+    "reg_alpha", # alias for parameter alpha
+    "lambdarank_pair_method", # rank only
+    "lambdarank_num_pair_per_sample", # rank only
+    "lambdarank_normalization", # rank only
+    "lambdarank_score_normalization", # rank only
+    "lambdarank_unbiased", # rank only
+    "lambdarank_bias_norm", # rank only
+    "ndcg_exp_gain", # rank only
+    "aft_loss_distribution", # only for AFT objective, not for Cox
+    "aft_loss_distribution_scale" # not in the doc?
   )
 
   paramtest_cox = run_paramtest(learner_cox, fun, exclude, tag = "train")
@@ -58,7 +38,7 @@ test_that("predict surv.xgboost", {
   learner_cox = lrn("surv.xgboost.cox")
   learner_aft = lrn("surv.xgboost.aft")
 
-  fun = xgboost:::predict.xgb.Booster # nolint
+  fun = xgboost:::predict.xgb.Booster
   exclude = c(
     "object", # handled by mlr3
     "newdata", # handled by mlr3
@@ -67,10 +47,10 @@ test_that("predict surv.xgboost", {
     "predcontrib", # always FALSE
     "approxcontrib", # unused
     "predinteraction", # always FALSE
-    "reshape", # handled internally
+    "strict_shape", # destroys prediction format
     "training", # always FALSE
-    "objective", # handled by mlr3
-    "ntreelimit" # doc says "deprecated, use 'iterationrange' instead"
+    "avoid_transpose", # always FALSE
+    "base_margin" # not supported
   )
 
   paramtest_cox = run_paramtest(learner_cox, fun, exclude, tag = "predict")
