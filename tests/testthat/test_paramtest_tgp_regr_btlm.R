2c27274aa7ed0ad163730af9ffe46ca04b9b3e4b
awinterstetter <A.Winterstetter@campus.lmu.de>
2026-01-02 09:45:23 +0100

    implementation of regr.btlm

---
+++ tests/testthat/test_paramtest_tgp_regr_btlm.R
@@ -0,0 +1,30 @@
+skip_if_not_installed("tgp")
+
+test_that("paramtest regr.btlm train", {
+  learner = lrn("regr.btlm")
+  fun = tgp::btlm
+  exclude = c(
+    "X", # handled via mlr3
+    "Z", # handled via mlr3
+    "XX", # handled via predict
+    "pred.n", # set internally to FALSE
+    "sens.p",
+    "MAP"
+  )
+  paramtest = run_paramtest(learner, fun, exclude, tag = "train")
+  expect_paramtest(paramtest)
+})
+
+test_that("paramtest regr.btlm predict", {
+  learner = lrn("regr.btlm")
+  fun = tgp:::predict.tgp
+  exclude = c(
+    "object", # handled via mlr3
+    "XX", # handled via mlr3
+    "pred.n", # set internally to FALSE
+    "sens.p",
+    "MAP"
+  )
+  paramtest = run_paramtest(learner, fun, exclude, tag = "predict")
+  expect_paramtest(paramtest)
+})
