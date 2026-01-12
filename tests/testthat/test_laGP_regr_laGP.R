be2a5184e059d32f813da767a595f9f999016a1c
awinterstetter <A.Winterstetter@campus.lmu.de>
2026-01-12 17:06:03 +0100

    removed browser()

--- tests/testthat/test_laGP_regr_laGP.R
+++ tests/testthat/test_laGP_regr_laGP.R
@@ -2,7 +2,6 @@ skip_if_not_installed("laGP")
 
 test_that("autotest", {
   learner = lrn("regr.laGP", end = 15) # I need to use sensible values here -> check with Marc?; with 15 it works
-  browser()
   expect_learner(learner)
   result = run_autotest(learner)
   expect_true(result)
