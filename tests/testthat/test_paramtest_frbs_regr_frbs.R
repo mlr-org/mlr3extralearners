8e2fd3669a563b3edabe7b917a52b0762ae9b380
awinterstetter <A.Winterstetter@campus.lmu.de>
2026-01-13 09:51:44 +0100

    excluded control-parameters in paramtest

--- tests/testthat/test_paramtest_frbs_regr_frbs.R
+++ tests/testthat/test_paramtest_frbs_regr_frbs.R
@@ -6,7 +6,29 @@ test_that("paramtest regr.frbs train", {
   exclude = c(
     "data.train", # handled by mlr3
     "range.data", # optional, computed from training data when omitted
-    "control" # constructed from learner parameters
+    "control", # constructed from learner parameters
+    "num.labels",  # handled by control function
+    "type.mf",  # handled by control function
+    "type.defuz",  # handled by control function
+    "type.tnorm",  # handled by control function
+    "type.snorm",  # handled by control function
+    "type.implication.func",  # handled by control function
+    "max.iter",  # handled by control function
+    "step.size",  # handled by control function
+    "r.a",  # handled by control function
+    "eps.high",  # handled by control function
+    "eps.low",  # handled by control function
+    "alpha.heuristic",  # handled by control function
+    "Dthr",  # handled by control function
+    "d",  # handled by control function
+    "persen_cross",  # handled by control function
+    "max.gen",  # handled by control function
+    "max.tune",  # handled by control function
+    "persen_mutant",  # handled by control function
+    "epsilon",  # handled by control function
+    "popu.size",  # handled by control function
+    "mode.tuning",  # handled by control function
+    "rule.selection"  # handled by control function
   )
 
   paramtest = run_paramtest(learner, fun_list, exclude, tag = "train")
@@ -18,7 +40,29 @@ test_that("paramtest regr.frbs predict", {
   fun_list = list(frbs:::predict.frbs) # nolint
   exclude = c(
     "object", # handled by mlr3
-    "newdata" # handled by mlr3
+    "newdata", # handled by mlr3
+        "num.labels",  # handled by control function
+    "type.mf",  # handled by control function
+    "type.defuz",  # handled by control function
+    "type.tnorm",  # handled by control function
+    "type.snorm",  # handled by control function
+    "type.implication.func",  # handled by control function
+    "max.iter",  # handled by control function
+    "step.size",  # handled by control function
+    "r.a",  # handled by control function
+    "eps.high",  # handled by control function
+    "eps.low",  # handled by control function
+    "alpha.heuristic",  # handled by control function
+    "Dthr",  # handled by control function
+    "d",  # handled by control function
+    "persen_cross",  # handled by control function
+    "max.gen",  # handled by control function
+    "max.tune",  # handled by control function
+    "persen_mutant",  # handled by control function
+    "epsilon",  # handled by control function
+    "popu.size",  # handled by control function
+    "mode.tuning",  # handled by control function
+    "rule.selection"  # handled by control function
   )
 
   paramtest = run_paramtest(learner, fun_list, exclude, tag = "predict")
