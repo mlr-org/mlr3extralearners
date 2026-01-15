1eebff19fd2c75f16d090b51ab62a9e60f2ac11b
be-marc <marcbecker@posteo.de>
2026-01-15 12:52:15 +0100

    ...

--- R/learner_tgp_regr_bgp.R
+++ R/learner_tgp_regr_bgp.R
@@ -26,35 +26,27 @@ LearnerRegrBgp = R6Class("LearnerRegrBgp",
     #' Creates a new instance of this [R6][R6::R6Class] class.
     initialize = function() {
       param_set = ps(
-        meanfn = p_fct(default = "linear", levels = c("constant", "linear"), tags = "train"),
         bprior = p_fct(default = "bflat", levels = c("b0", "b0not", "bflat", "bmle", "bmznot", "bmzt"), tags = "train"),
-        corr = p_fct(default = "expsep", levels = c("exp", "expsep", "matern", "sim"), tags = "train"),
-        BTE = p_uty(
-          default = c(1000L, 4000L, 2L),
-          tags = c("train", "predict"),
-          custom_check = mlr3misc::crate(function(x) {
-            if (!checkmate::test_integerish(x, len = 3, lower = 0)) {
-              return("`BTE` must be an integerish vector of length 3 with non-negative entries")
-            }
-            TRUE
-          })
-        ),
-        R = p_int(default = 1L, lower = 1L, tags = c("train", "predict")),
-        m0r1 = p_lgl(default = TRUE, tags = "train"),
-        itemps = p_uty(default = NULL, tags = "train"),
-        krige = p_lgl(default = TRUE, tags = c("train", "predict")),
-        zcov = p_lgl(default = FALSE, tags = c("train", "predict")),
-        Ds2x = p_lgl(default = FALSE, tags = c("train", "predict")),
+        BTE = p_uty(default = c(1000L, 4000L, 2L), tags = c("train", "predict"), custom_check = mlr3misc::crate({function(x) {
+          if (!checkmate::test_integerish(x, len = 3, lower = 0)) {
+            return("`BTE` must be an integerish vector of length 3 with non-negative entries")
+          }
+          TRUE
+        }})),
+        corr   = p_fct(default = "expsep", levels = c("exp", "expsep", "matern", "sim"), tags = "train"),
+        Ds2x   = p_lgl(default = FALSE, tags = c("train", "predict")),
         improv = p_lgl(default = FALSE, tags = c("train", "predict")),
+        itemps = p_uty(default = NULL, tags = "train"),
+        krige  = p_lgl(default = TRUE, tags = c("train", "predict")),
+        m0r1   = p_lgl(default = TRUE, tags = "train"),
+        MAP    = p_lgl(default = TRUE, tags = "predict"),
+        meanfn = p_fct(default = "linear", levels = c("constant", "linear"), tags = "train"),
+        nu     = p_dbl(default = 1.5, tags = "train", depends = quote(corr == "matern")),
+        R      = p_int(default = 1L, lower = 1L, tags = c("train", "predict")),
         sens.p = p_uty(default = NULL, tags = c("train", "predict")),
-        nu = p_dbl(default = 1.5, tags = "train", depends = quote(corr == "matern")),
-        MAP = p_lgl(default = TRUE, tags = "predict"),
-        trace = p_lgl(default = FALSE, tags = c("train", "predict")),
-        verb = p_int(default = 1L, lower = 0L, upper = 4L, tags = c("train", "predict"))
-      )
-
-      param_set$values = list(
-        verb = 0L
+        trace  = p_lgl(default = FALSE, tags = c("train", "predict")),
+        verb   = p_int(init= 0L, lower = 0L, upper = 4L, tags = c("train", "predict")),
+        zcov   = p_lgl(default = FALSE, tags = c("train", "predict"))
       )
 
       super$initialize(
@@ -72,12 +64,10 @@ LearnerRegrBgp = R6Class("LearnerRegrBgp",
     .train = function(task) {
       pars = self$param_set$get_values(tags = "train")
       data = as_numeric_matrix(task$data(cols = task$feature_names))
-      target = task$truth()
 
-      mlr3misc::invoke(
-        tgp::bgp,
+      mlr3misc::invoke(tgp::bgp,
         X = data,
-        Z = target,
+        Z = task$truth(),
         .args = pars
       )
     },
@@ -86,9 +76,8 @@ LearnerRegrBgp = R6Class("LearnerRegrBgp",
       pars = self$param_set$get_values(tags = "predict")
       newdata = as_numeric_matrix(ordered_features(task, self))
 
-      pred = mlr3misc::invoke(
-        predict,
-        self$model,
+      pred = mlr3misc::invoke(predict,
+        object = self$model,
         XX = newdata,
         .args = pars
       )
