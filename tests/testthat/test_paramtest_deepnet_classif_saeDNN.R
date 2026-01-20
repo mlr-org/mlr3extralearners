326d06f18ec6c9ad853cbf0d0a312fb265429f9a
awinterstetter <A.Winterstetter@campus.lmu.de>
2026-01-20 11:01:35 +0100

    implementation of classif.saeDNN

---
+++ tests/testthat/test_paramtest_deepnet_classif_saeDNN.R
@@ -0,0 +1,23 @@
+skip_if_not_installed("deepnet")
+
+test_that("paramtest classif.saeDNN train", {
+  learner = lrn("classif.saeDNN")
+  fun = deepnet::sae.dnn.train
+  exclude = c(
+    "x", # handled internally
+    "y" # handled internally
+  )
+  paramtest = run_paramtest(learner, fun, exclude, tag = "train")
+  expect_paramtest(paramtest)
+})
+
+test_that("paramtest classif.saeDNN predict", {
+  learner = lrn("classif.saeDNN")
+  fun = deepnet::nn.predict
+  exclude = c(
+    "nn", # handled internally
+    "x" # handled internally
+  )
+  paramtest = run_paramtest(learner, fun, exclude, tag = "predict")
+  expect_paramtest(paramtest)
+})
