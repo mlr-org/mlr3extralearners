e8278fe6292e828351a830ac179451391273e1f7
be-marc <marcbecker@posteo.de>
2026-02-03 08:10:02 +0100

    ...

--- tests/testthat/teardown.R
+++ tests/testthat/teardown.R
@@ -1,6 +1,11 @@
 options(old_opts)
 lg = lgr::get_logger("mlr3")
 lg$set_threshold(old_threshold)
-unlink(list.files(tempdir(), full.names = TRUE), recursive = TRUE, force = TRUE)
 
 mirai::daemons(0, .compute = "mlr3_encapsulation")
+
+if (requireNamespace("reticulate", quietly = TRUE)) {
+  python_temp_dir = dirname(reticulate::py_run_string("import tempfile; x=tempfile.NamedTemporaryFile().name", local = TRUE)$x)
+  detritus = list.files(python_temp_dir, pattern = "^uv-setuptools-[0-9A-Fa-f]{16}\\.lock$", full.names = TRUE)
+  if (length(detritus)) unlink(detritus)
+}
