92d575eeadecb8cc37b5e3a84779356eca27bc7d
be-marc <marcbecker@posteo.de>
2026-02-04 12:22:37 +0100

    ...

--- tests/testthat/teardown.R
+++ tests/testthat/teardown.R
@@ -5,6 +5,7 @@ lg$set_threshold(old_threshold)
 mirai::daemons(0, .compute = "mlr3_encapsulation")
 
 if (requireNamespace("reticulate", quietly = TRUE)) {
+  # created by reticulate and uv
   python_temp_dir = dirname(reticulate::py_run_string("import tempfile; x=tempfile.NamedTemporaryFile().name", local = TRUE)$x)
   detritus = list.files(python_temp_dir, pattern = "^uv-setuptools-[0-9A-Fa-f]{16}\\.lock$", full.names = TRUE)
   if (length(detritus)) unlink(detritus)
@@ -15,5 +16,7 @@ if (requireNamespace("reticulate", quietly = TRUE)) {
 
   # created by fastai test
   detritus = list.files(python_temp_dir, pattern = "^tmp[a-z0-9]{8}$", full.names = TRUE)
+  if (length(detritus)) unlink(detritus, recursive = TRUE, force = TRUE)
+  detritus = list.files(python_temp_dir, pattern = "^tmp_[a-z0-9]{7}$", full.names = TRUE)
   if (length(detritus)) unlink(detritus, recursive = TRUE, force = TRUE)
 }
