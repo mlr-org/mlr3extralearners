5a44d147b17513f669f54b4695ba6bcff28af4fd
awinterstetter <A.Winterstetter@campus.lmu.de>
2026-02-24 09:56:43 +0100

    removed mirai::everywhere

--- tests/testthat/test_h2o_classif_deeplearning.R
+++ tests/testthat/test_h2o_classif_deeplearning.R
@@ -1,14 +1,11 @@
+
+
 test_that("autotest", {
   skip_if_not_installed("h2o")
 
   compute = "mlr3_encapsulation_h2o"
   mirai::daemons(1, .compute = compute)
   on.exit(mirai::daemons(0, .compute = compute))
-
-  mirai::everywhere({
-    library(h2o)
-    h2o::h2o.init()
-  }, .compute = compute)
 
   learner = lrn("classif.h2o.deeplearning", epochs = 10, hidden = c(20L, 20L), seed = 42L, reproducible = TRUE)
   expect_learner(learner)
