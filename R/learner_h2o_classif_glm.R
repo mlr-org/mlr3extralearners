228c2e7bd3f12f81d68c9a772d90a7a991c759fa
awinterstetter <A.Winterstetter@campus.lmu.de>
2026-02-24 14:49:46 +0100

    added parameters + refined autotest + refined Learner code

--- R/learner_h2o_classif_glm.R
+++ R/learner_h2o_classif_glm.R
@@ -4,14 +4,13 @@
 #'
 #' @description
 #' Generalized linear classification model.
-#' Calls [h2o::h2o.glm()] from package \CRANpkg{h2o} with `family`
-#' always set to `"binomial"`.
+#' Calls [h2o::h2o.glm()] from package \CRANpkg{h2o} with `family` always set to `"binomial"`.
 #'
 #' @section Initial parameter values:
 #' - `family`
 #'   - Fixed to `"binomial"` by this learner wrapper.
 #' - `missing_values_handling`
-#'   - Not exposed; H2O default `"MeanImputation"` is used.
+#'   - H2O default is `"MeanImputation"`.
 #'
 #' @templateVar id classif.h2o.glm
 #' @template learner
@@ -30,19 +29,49 @@ LearnerClassifH2OGLM = R6Class("LearnerClassifH2OGLM", inherit = LearnerClassif,
     initialize = function() {
       param_set = ps(
         max_iterations = p_int(lower = 0L, default = 50L, tags = "train"),
+        seed = p_int(tags = "train"),
         beta_epsilon = p_dbl(lower = 0, default = 0, tags = "train"),
+        objective_epsilon = p_dbl(lower = 0, tags = "train"),
+        gradient_epsilon = p_dbl(lower = 0, tags = "train"),
         standardize = p_lgl(default = TRUE, tags = "train"),
-        solver = p_fct(levels = c("IRLSM", "L_BFGS"), default = "IRLSM", tags = "train"),
-        link = p_fct(levels = c("logit", "log"), default = "logit", tags = "train"),
+        solver = p_fct(levels = c("AUTO", "IRLSM", "L_BFGS", "COORDINATE_DESCENT", "COORDINATE_DESCENT_NAIVE"), default = "IRLSM", tags = "train"),
+        link = p_fct(levels = c("family_default", "logit", "log"), default = "logit", tags = "train"),
+        ignore_const_cols = p_lgl(tags = "train"),
+        score_each_iteration = p_lgl(tags = "train"),
+        score_iteration_interval = p_int(tags = "train"),
         alpha = p_dbl(lower = 0, upper = 1, default = 0.5, tags = "train"),
         prior = p_dbl(lower = 0, tags = "train"),
         lambda = p_dbl(lower = 0, default = 1e-5, tags = "train"),
         lambda_search = p_lgl(default = FALSE, tags = "train"),
         nlambdas = p_int(lower = 1L, depends = quote(lambda_search == TRUE), tags = "train"),
         lambda_min_ratio = p_dbl(lower = 0, upper = 1, depends = quote(lambda_search == TRUE), tags = "train"),
+        early_stopping = p_lgl(tags = "train"),
+        stopping_rounds = p_int(lower = 0L, tags = "train"),
+        stopping_metric = p_uty(tags = "train"),
+        stopping_tolerance = p_dbl(lower = 0, tags = "train"),
+        missing_values_handling = p_fct(levels = c("MeanImputation", "Skip", "PlugValues"), tags = "train"),
+        plug_values = p_uty(depends = quote(missing_values_handling == "PlugValues"), tags = "train"),
+        compute_p_values = p_lgl(tags = "train"),
+        remove_collinear_columns = p_lgl(tags = "train"),
         beta_constraints = p_uty(tags = "train"),
-        intercept = p_lgl(default = TRUE, tags = "train"),
-        quiet = p_lgl(init = TRUE, tags = c("train", "predict"))
+        non_negative = p_lgl(tags = "train"),
+        startval = p_uty(tags = "train"),
+        calc_like = p_lgl(tags = "train"),
+        cold_start = p_lgl(tags = "train"),
+        max_active_predictors = p_int(tags = "train"),
+        interactions = p_uty(tags = "train"),
+        interaction_pairs = p_uty(tags = "train"),
+        obj_reg = p_dbl(lower = 0, tags = "train"),
+        balance_classes = p_lgl(tags = "train"),
+        class_sampling_factors = p_uty(depends = quote(balance_classes == TRUE), tags = "train"),
+        max_after_balance_size = p_dbl(lower = 0, depends = quote(balance_classes == TRUE), tags = "train"),
+        max_runtime_secs = p_dbl(lower = 0, tags = "train"),
+        generate_scoring_history = p_lgl(tags = "train"),
+        build_null_model = p_lgl(tags = "train"),
+        generate_variable_inflation_factors = p_lgl(tags = "train"),
+        gainslift_bins = p_int(tags = "train"),
+        init_optimal_glm = p_lgl(tags = "train"),
+        intercept = p_lgl(default = TRUE, tags = "train")
       )
 
       super$initialize(
@@ -51,28 +80,10 @@ LearnerClassifH2OGLM = R6Class("LearnerClassifH2OGLM", inherit = LearnerClassif,
         feature_types = c("integer", "numeric", "factor"),
         predict_types = c("response", "prob"),
         param_set = param_set,
-        properties = c("weights", "twoclass", "missings", "importance"),
+        properties = c("weights", "twoclass", "missings"),
         man = "mlr3extralearners::mlr_learners_classif.h2o.glm",
         label = "H2O GLM"
       )
-    },
-
-    #' @description
-    #' Variable importance scores computed by H2O.
-    #' @return Named `numeric()`.
-    importance = function() {
-      if (is.null(self$model)) {
-        stopf("No model stored")
-      }
-
-      imp = stats::na.omit(as.data.frame(h2o::h2o.varimp(self$model)))
-      if (is.null(imp) || !nrow(imp)) {
-        stop("No importance available from H2O.")
-      }
-
-      scores = imp$relative_importance
-      names(scores) = imp$variable
-      sort(scores, decreasing = TRUE)
     }
   ),
 
@@ -80,11 +91,9 @@ LearnerClassifH2OGLM = R6Class("LearnerClassifH2OGLM", inherit = LearnerClassif,
 
     .train = function(task) {
 
-      conn.up = tryCatch(h2o::h2o.getConnection(), error = function(err) {
-        FALSE
-      })
+      conn.up = try(h2o::h2o.getConnection())
       if (!inherits(conn.up, "H2OConnection")) {
-        h2o::h2o.init()
+        invisible(capture.output(h2o::h2o.init(ip = "127.0.0.1")))
       }
 
       pars = self$param_set$get_values(tags = "train")
@@ -98,73 +107,42 @@ LearnerClassifH2OGLM = R6Class("LearnerClassifH2OGLM", inherit = LearnerClassif,
         pars$weights_column = ".mlr_weights"
       }
 
-      quiet = pars$quiet
-      pars$quiet = NULL
-
       pars$family = "binomial"
-      training_frame = h2o::as.h2o(data)
+      training_frame = h2o::h2o.no_progress(h2o::as.h2o(data))
 
-      train_fun = function() {
-        invoke(h2o::h2o.glm,
+       h2o::h2o.no_progress(invoke(h2o::h2o.glm,
           y = target,
           x = features,
           training_frame = training_frame,
           .args = pars
-        )
-      }
-
-      if (quiet == TRUE) {
-        utils::capture.output({
-          model = train_fun()
-        })
-      } else {
-        model = train_fun()
-      }
-
-      model
+      ))
     },
 
     .predict = function(task) {
 
-      conn.up = tryCatch(h2o::h2o.getConnection(), error = function(err) {
-        FALSE
-      })
+      conn.up = try(h2o::h2o.getConnection())
       if (!inherits(conn.up, "H2OConnection")) {
-        h2o::h2o.init()
+        invisible(capture.output(h2o::h2o.init(ip = "127.0.0.1")))
       }
 
-      newdata = h2o::as.h2o(ordered_features(task, self))
+      newdata = h2o::h2o.no_progress(h2o::as.h2o(ordered_features(task, self)))
 
-      pars = self$param_set$get_values(tags = "predict")
-      quiet = pars$quiet
+      pred = h2o::h2o.no_progress(h2o::h2o.predict(self$model, newdata = newdata))
 
-      pred_fun = function() {
-        h2o::h2o.predict(self$model, newdata = newdata)
-      }
-
-      if (quiet == TRUE) {
-        utils::capture.output({
-          pred = pred_fun()
-        })
-      } else {
-        pred = pred_fun()
-      }
-
-      pred_df = as.data.frame(pred)
-      response = factor(as.character(pred_df$predict), levels = task$class_names)
+      response = factor(as.vector(pred$predict), levels = task$class_names)
 
       if (self$predict_type == "response") {
         return(list(response = response))
       }
 
-      prob = as.matrix(pred_df[, -1L, drop = FALSE])
-      prob_names = names(pred_df)[-1L]
+      prob = as.matrix(pred[, -1L])
+      prob_names = names(pred)[-1L]
 
-      if (all(grepl("^p[[:digit:]]+$", prob_names))) {
-        stripped = sub("^p", "", prob_names)
-        if (all(task$class_names %in% stripped)) {
-          prob_names = stripped
-        }
+      # H2O-predict may return probability columns as p0, p1, etc. for integer-like class labels, so we need to strip the p to create final prob-table correctly
+      int_response = grepl("^[[:digit:]]+$", as.character(as.vector(pred$predict)))
+      p_cols = grepl("^p[[:digit:]]+$", prob_names)
+      if (any(int_response) && any(p_cols)) {
+        prob_names[p_cols] = sub("^p", "", prob_names[p_cols])
       }
 
       colnames(prob) = prob_names
--- tests/testthat/test_h2o_classif_glm.R
+++ tests/testthat/test_h2o_classif_glm.R
@@ -1,20 +1,11 @@
+skip_if_not_installed("h2o")
+
 test_that("autotest", {
-  skip_if_not_installed("h2o")
-
-  compute = "mlr3_encapsulation"
-  mirai::daemons(1, .compute = compute)
-
-  mirai::everywhere({
-    library(h2o)
-    Sys.unsetenv(c("http_proxy", "https_proxy", "HTTP_PROXY", "HTTPS_PROXY"))
-    h2o::h2o.init(ip = "localhost", startH2O = TRUE)
-  }, .compute = compute)
 
   learner = lrn("classif.h2o.glm", max_iterations = 50L, lambda = 1e-5)
   expect_learner(learner)
 
-  result = run_autotest(learner, check_replicable = FALSE, exclude = "feat_all|feat_single_integer_binary")
-  # H2O GLM internally expands factor features before computing importances.
-  # In some H2O versions, single-integer-feature binary prediction can trigger a Java NPE.
+  result = run_autotest(learner)
+
   expect_true(result, info = result$error)
 })
