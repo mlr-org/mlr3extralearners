bae3c7cf14017f4b3762c75212bb2ad2e7a9e081
awinterstetter <A.Winterstetter@campus.lmu.de>
2026-03-03 07:27:15 +0100

    style changes

--- R/learner_h2o_regr_gbm.R
+++ R/learner_h2o_regr_gbm.R
@@ -22,45 +22,45 @@ LearnerRegrH2OGBM = R6Class("LearnerRegrH2OGBM", inherit = LearnerRegr,
     #' Creates a new instance of this [R6][R6::R6Class] class.
     initialize = function() {
       param_set = ps(
-        ntrees = p_int(default = 50L, lower = 1L, tags = "train"),
-        max_depth = p_int(default = 5L, lower = 1L, tags = "train"),
-        min_rows = p_int(default = 10L, lower = 1L, tags = "train"),
-        nbins = p_int(default = 20L, lower = 1L, tags = "train"),
-        nbins_cats = p_int(default = 1024L, lower = 1L, tags = "train"),
-        nbins_top_level = p_int(default = 1024L, lower = 1L, tags = "train"),
-        seed = p_int(default = -1L, tags = "train"),
-        learn_rate = p_dbl(default = 0.1, lower = 0, upper = 1, tags = "train"),
-        learn_rate_annealing = p_dbl(default = 1, lower = 0, upper = 1, tags = "train"),
-        distribution = p_fct(default = "gaussian", levels = c("AUTO", "poisson", "laplace", "tweedie", "gaussian", "huber", "gamma", "quantile", "custom"), tags = "train"),
-        sample_rate = p_dbl(default = 1, lower = 0, upper = 1, tags = "train"),
+        auto_rebalance = p_lgl(default = TRUE, tags = "train"),
+        build_tree_one_node = p_lgl(default = FALSE, tags = "train"),
+        categorical_encoding = p_fct(default = "AUTO", levels = c("AUTO", "Enum", "OneHotInternal", "OneHotExplicit", "Binary", "Eigen", "LabelEncoder", "SortByResponse", "EnumLimited"), tags = "train"),
+        check_constant_response = p_lgl(default = TRUE, tags = "train"),
+        checkpoint = p_uty(tags = "train"),
         col_sample_rate = p_dbl(default = 1, lower = 0, upper = 1, tags = "train"),
         col_sample_rate_change_per_level = p_dbl(default = 1, lower = 0, upper = 2, tags = "train"),
         col_sample_rate_per_tree = p_dbl(default = 1, lower = 0, upper = 1, tags = "train"),
-        max_abs_leafnode_pred = p_dbl(default = Inf, lower = 0, tags = "train"),
-        pred_noise_bandwidth = p_dbl(default = 0, lower = 0, tags = "train"),
-        categorical_encoding = p_fct(default = "AUTO", levels = c("AUTO", "Enum", "OneHotInternal", "OneHotExplicit", "Binary", "Eigen", "LabelEncoder", "SortByResponse", "EnumLimited"), tags = "train"),
-        min_split_improvement = p_dbl(default = 1e-05, lower = 0, tags = "train"),
-        histogram_type = p_fct(default = "AUTO", levels = c("AUTO", "UniformAdaptive", "Random", "QuantilesGlobal", "RoundRobin", "UniformRobust"), tags = "train"),
-        score_each_iteration = p_lgl(default = FALSE, tags = "train"),
-        score_tree_interval = p_int(default = 0L, lower = 0L, tags = "train"),
-        ignore_const_cols = p_lgl(default = TRUE, tags = "train"),
-        stopping_rounds = p_int(default = 0L, lower = 0L, tags = "train"),
-        stopping_metric = p_fct(default = "AUTO", levels = c("AUTO", "deviance", "MSE", "RMSE", "MAE", "RMSLE"), tags = "train"),
-        stopping_tolerance = p_dbl(default = 0.001, lower = 0, tags = "train"),
-        max_runtime_secs = p_dbl(default = 0, lower = 0, tags = "train"),
-        build_tree_one_node = p_lgl(default = FALSE, tags = "train"),
-        quantile_alpha = p_dbl(default = 0.5, lower = 0, upper = 1, depends = quote(distribution == "quantile"), tags = "train"),
-        tweedie_power = p_dbl(default = 1.5, lower = 1, upper = 2, depends = quote(distribution == "tweedie"), tags = "train"),
-        huber_alpha = p_dbl(default = 0.9, lower = 0, upper = 1, depends = quote(distribution == "huber"), tags = "train"),
-        checkpoint = p_uty(tags = "train"),
+        custom_distribution_func = p_uty(default = NULL, depends = quote(distribution == "custom"), tags = "train"),
+        distribution = p_fct(default = "gaussian", levels = c("AUTO", "poisson", "laplace", "tweedie", "gaussian", "huber", "gamma", "quantile", "custom"), tags = "train"),
         export_checkpoints_dir = p_uty(default = NULL, tags = "train"),
+        histogram_type = p_fct(default = "AUTO", levels = c("AUTO", "UniformAdaptive", "Random", "QuantilesGlobal", "RoundRobin", "UniformRobust"), tags = "train"),
+        huber_alpha = p_dbl(default = 0.9, lower = 0, upper = 1, depends = quote(distribution == "huber"), tags = "train"),
+        ignore_const_cols = p_lgl(default = TRUE, tags = "train"),
         in_training_checkpoints_dir = p_uty(default = NULL, tags = "train"),
         in_training_checkpoints_tree_interval = p_int(default = 1L, lower = 1L, tags = "train"),
-        monotone_constraints = p_uty(default = NULL, tags = "train"),
-        check_constant_response = p_lgl(default = TRUE, tags = "train"),
         interaction_constraints = p_uty(default = NULL, tags = "train"),
-        auto_rebalance = p_lgl(default = TRUE, tags = "train"),
-        custom_distribution_func = p_uty(default = NULL, depends = quote(distribution == "custom"), tags = "train"),
+        learn_rate = p_dbl(default = 0.1, lower = 0, upper = 1, tags = "train"),
+        learn_rate_annealing = p_dbl(default = 1, lower = 0, upper = 1, tags = "train"),
+        max_abs_leafnode_pred = p_dbl(default = Inf, lower = 0, tags = "train"),
+        max_depth = p_int(default = 5L, lower = 1L, tags = "train"),
+        max_runtime_secs = p_dbl(default = 0, lower = 0, tags = "train"),
+        min_rows = p_int(default = 10L, lower = 1L, tags = "train"),
+        min_split_improvement = p_dbl(default = 1e-05, lower = 0, tags = "train"),
+        monotone_constraints = p_uty(default = NULL, tags = "train"),
+        nbins = p_int(default = 20L, lower = 1L, tags = "train"),
+        nbins_cats = p_int(default = 1024L, lower = 1L, tags = "train"),
+        nbins_top_level = p_int(default = 1024L, lower = 1L, tags = "train"),
+        ntrees = p_int(default = 50L, lower = 1L, tags = "train"),
+        pred_noise_bandwidth = p_dbl(default = 0, lower = 0, tags = "train"),
+        quantile_alpha = p_dbl(default = 0.5, lower = 0, upper = 1, depends = quote(distribution == "quantile"), tags = "train"),
+        sample_rate = p_dbl(default = 1, lower = 0, upper = 1, tags = "train"),
+        score_each_iteration = p_lgl(default = FALSE, tags = "train"),
+        score_tree_interval = p_int(default = 0L, lower = 0L, tags = "train"),
+        seed = p_int(default = -1L, tags = "train"),
+        stopping_metric = p_fct(default = "AUTO", levels = c("AUTO", "deviance", "MSE", "RMSE", "MAE", "RMSLE"), tags = "train"),
+        stopping_rounds = p_int(default = 0L, lower = 0L, tags = "train"),
+        stopping_tolerance = p_dbl(default = 0.001, lower = 0, tags = "train"),
+        tweedie_power = p_dbl(default = 1.5, lower = 1, upper = 2, depends = quote(distribution == "tweedie"), tags = "train"),
         verbose = p_lgl(default = FALSE, tags = "train")
       )
 
@@ -80,15 +80,11 @@ LearnerRegrH2OGBM = R6Class("LearnerRegrH2OGBM", inherit = LearnerRegr,
   private = list(
 
     .train = function(task) {
-
-      conn.up = try(h2o::h2o.getConnection())
-      if (!inherits(conn.up, "H2OConnection")) {
+      if (!inherits(try(h2o::h2o.getConnection(), silent = TRUE), "H2OConnection")) {
         invisible(capture.output(h2o::h2o.init(ip = "127.0.0.1")))
       }
 
       pars = self$param_set$get_values(tags = "train")
-      target = task$target_names
-      features = task$feature_names
       data = task$data()
 
       weights = private$.get_weights(task)
@@ -99,9 +95,10 @@ LearnerRegrH2OGBM = R6Class("LearnerRegrH2OGBM", inherit = LearnerRegr,
 
       training_frame = h2o::h2o.no_progress(h2o::as.h2o(data))
 
-      h2o::h2o.no_progress(invoke(h2o::h2o.gbm,
-          y = target,
-          x = features,
+      h2o::h2o.no_progress(invoke(
+          .f = h2o::h2o.gbm,
+          y = task$target_names,
+          x = task$feature_names,
           training_frame = training_frame,
           .args = pars
         )
@@ -109,8 +106,7 @@ LearnerRegrH2OGBM = R6Class("LearnerRegrH2OGBM", inherit = LearnerRegr,
     },
 
     .predict = function(task) {
-      conn.up = try(h2o::h2o.getConnection())
-      if (!inherits(conn.up, "H2OConnection")) {
+      if (!inherits(try(h2o::h2o.getConnection(), silent = TRUE), "H2OConnection")) {
         invisible(capture.output(h2o::h2o.init(ip = "127.0.0.1")))
       }
 
