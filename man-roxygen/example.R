7e2f6c12228c97f4d095fbdc462dee794a4ffa0d
awinterstetter <A.Winterstetter@campus.lmu.de>
2026-03-18 12:22:40 +0100

    removed learner_args part

--- man-roxygen/example.R
+++ man-roxygen/example.R
@@ -1,7 +1,6 @@
 <%
 pkgs = setdiff(lrn(id)$packages, c("mlr3", "mlr3learners"))
 l  = lrn(id)
-learner_args = if (exists("example_learner_args") && nzchar(example_learner_args)) sprintf(", %s", example_learner_args) else ""
 task_id = if ("LearnerClassif" %in% class(l)) {
 "sonar"
 } else if ("LearnerRegr" %in% class(l)) {
@@ -13,7 +12,7 @@ task_id = if ("LearnerClassif" %in% class(l)) {
 #'
 #' <%= sprintf("@examplesIf learner_is_runnable(\"%s\")", id) %>
 #' # Define the Learner
-#' <%= sprintf("learner = lrn(\"%s\"%s)", id, learner_args)%>
+#' <%= sprintf("learner = lrn(\"%s\")", id)%>
 #' print(learner)
 #'
 #' # Define a Task
