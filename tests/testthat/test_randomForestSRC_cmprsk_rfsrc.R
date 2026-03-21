d2e5a63f3d83399a9835d091de45e262607a55b2
john <bblodfon@gmail.com>
2026-03-21 23:27:03 +0100

    remove warnings supression code as cmprsk.aalen supports these properties now

--- tests/testthat/test_randomForestSRC_cmprsk_rfsrc.R
+++ tests/testthat/test_randomForestSRC_cmprsk_rfsrc.R
@@ -7,12 +7,7 @@ test_that("autotest", {
   # remove property as prediction doesn't work due to rsfrc bug
   learner$properties = setdiff(learner$properties, "selected_features")
 
-  # suppress expected warnings during encapsulation autotest from fallback learner
-  # `cmprsk.aalen`, which lacks the 'missings' and 'importance' properties of
-  # `cmprsk.rfsrc`
-  result = suppress_fallback_warnings(
-    run_autotest(learner, N = 42, check_replicable = FALSE)
-  )
+  result = run_autotest(learner, N = 42, check_replicable = FALSE)
   expect_true(result, info = result$error)
 })
 
