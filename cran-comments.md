3271d202b05bdcc0dcd2ce1a8ab32dcf80e51d42
be-marc <marcbecker@posteo.de>
2026-04-02 11:58:35 +0200

    release: 1.5.0

---
+++ cran-comments.md
@@ -0,0 +1,11 @@
+## Test environments
+
+- local: R version x.y.z, Platform
+
+## R CMD check results
+
+0 errors | 0 warnings | 0 notes
+
+## Reverse dependencies
+
+Checked with revdepcheck::revdep_check(). No new problems.
--- man-roxygen/seealso_learner.R
+++ man-roxygen/seealso_learner.R
@@ -1,7 +1,7 @@
 #' @seealso
 #' * [Dictionary][mlr3misc::Dictionary] of [Learners][mlr3::Learner]: [mlr3::mlr_learners].
 #' * `as.data.table(mlr_learners)` for a table of available [Learners][mlr3::Learner] in the running session (depending on the loaded packages).
-#' * Chapter in the [mlr3book](https://mlr3book.mlr-org.com/): \url{https://mlr3book.mlr-org.com/basics.html#learners}
+#' * Chapter in the [mlr3book](https://mlr3book.mlr-org.com/): \url{https://mlr3book.mlr-org.com/chapters/chapter2/data_and_basic_modeling.html#sec-learners}
 #' * \CRANpkg{mlr3learners} for a selection of recommended learners.
 #' * \CRANpkg{mlr3cluster} for unsupervised clustering learners.
 #' * \CRANpkg{mlr3pipelines} to combine learners with pre- and postprocessing steps.
