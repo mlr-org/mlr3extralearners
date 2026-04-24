5191b3b4038e74d46c731e243d4cc358f2a18320
saba5482 <ton.email@ecole.com>
2026-04-24 16:46:37 -0400

    Fix R6 structure, add parameters and autotests

--- R/learner_grf_regr_regression_forest.R
+++ R/learner_grf_regr_regression_forest.R
@@ -15,23 +15,27 @@ LearnerRegrRegressionForest <- R6::R6Class("LearnerRegrRegressionForest",
       super$initialize(
         id = "regr.regression_forest",
         packages = "grf",
-        feature_types = c("logical", "integer", "numeric"),
+        feature_types = c("integer", "numeric"),
         predict_types = c("response"),
-        param_set = paradox::ps(),
+        param_set = paradox::ps(
+          num.trees = paradox::p_int(default = 2000, lower = 1, tags = "train"),
+          min.node.size = paradox::p_int(default = 5, lower = 1, tags = "train"),
+          sample.fraction = paradox::p_dbl(default = 0.5, lower = 0, upper = 1, tags = "train")
+        ),
         properties = character(0)
       )
     }
-  ),
+  ), 
   private = list(
     .train = function(task) {
+      pars = self$param_set$get_values(tags = "train")
       X_mat <- as.matrix(task$data(cols = task$feature_names))
-      Y_vec <- task$data(cols = task$target_names)[[1]]
+      Y_vec <- as.numeric(task$data(cols = task$target_names)[[1]])
       
-      grf::regression_forest(X = X_mat, Y = Y_vec)
-    },
+      mlr3misc::invoke(grf::regression_forest, X = X_mat, Y = Y_vec, .args = pars)
+    }, 
     .predict = function(task) {
       X_mat <- as.matrix(task$data(cols = task$feature_names))
-      
       prediction.result <- predict(self$model, X_mat)
       
       mlr3::PredictionRegr$new(
--- tests/testthat/test_grf_regr_regression_forest.R
+++ tests/testthat/test_grf_regr_regression_forest.R
@@ -1,11 +1,18 @@
+test_that("autotest", {
+  learner = LearnerRegrRegressionForest$new()
+  expect_learner(learner)
+  result = run_autotest(learner, exclude = "feat_all")
+  expect_true(result, info = result$error)
+})
+
 test_that("train and predict work for regression_forest", {
   task.mtcars <- mlr3::tsk("mtcars")
   learner.grf <- LearnerRegrRegressionForest$new()
   
   learner.grf$train(task.mtcars)
-  expect_true(!is.null(learner.grf$model))
+  expect_false(is.null(learner.grf$model))
   
   prediction.result <- learner.grf$predict(task.mtcars)
-  expect_true(inherits(prediction.result, "PredictionRegr"))
+  expect_s3_class(prediction.result, "PredictionRegr")
   expect_equal(length(prediction.result$response), task.mtcars$nrow)
 })
