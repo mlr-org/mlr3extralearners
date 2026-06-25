d70cc046479c73742103bf84e0e4bea442fe31d7
john <bblodfon@gmail.com>
2026-06-25 18:24:51 +0200

    add seed to test

--- tests/testthat/test_mgcv_surv_gam_cox.R
+++ tests/testthat/test_mgcv_surv_gam_cox.R
@@ -1,6 +1,7 @@
 skip_if_not_installed("mgcv")
 
 test_that("autotest", {
+  withr::local_seed(42)
   learner = lrn("surv.gam.cox")
   expect_learner(learner)
   result = run_autotest(learner, exclude = "utf8_feature_names")
