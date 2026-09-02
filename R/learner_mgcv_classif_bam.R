mlr-org/mlr3extralearners

commit f2d5162bb92ac2347c6dd5290ca8a9ba87482e66

fix: change ties.method to 'first' for deterministic predictions

R/learner_mgcv_classif_bam.R
@@ -126,7 +126,7 @@ LearnerClassifBam <- R6::R6Class(
       colnames(model_pred) = task$class_names
 
       if (self$predict_type == "response") {
-        class_indices = max.col(model_pred, ties.method = "random")
+        class_indices = max.col(model_pred, ties.method = "first")
         response = colnames(model_pred)[class_indices]
         list(response = unname(response))
       } else {

commit b9e992cd9801e25b02a085a25755e79afe219efa

fix(classif.bam): keep ties.method = "first"
Restores the deterministic tie breaking introduced in f2d5162bb, which was
requested during review of the pull request.

R/learner_mgcv_classif_bam.R
@@ -137,7 +137,8 @@ LearnerClassifBam = R6Class("LearnerClassifBam",
       colnames(prob) = lvls
 
       if (self$predict_type == "response") {
-        i = max.col(prob, ties.method = "random")
+        # `first` rather than the `max.col()` default `random`, so that predictions are reproducible
+        i = max.col(prob, ties.method = "first")
         list(response = factor(colnames(prob)[i], levels = lvls))
       } else {
         list(prob = prob)